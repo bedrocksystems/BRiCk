09c9c07ae3b103d28ee2913e7433f80d1c0d7e2f
Gregory Malecha <gmalecha@gmail.com>
2019-01-03 16:26:05 -0500

    notes about default constructors.

--- src/ToCoq.cpp
+++ src/ToCoq.cpp
@@ -823,6 +823,7 @@ private:
 		  PRINT_LIST(cd->param, parent->printParam);
 		  output() << fmt::line << " ; c_body :=" << fmt::nbsp << fmt::indent << fmt::indent;
 		  if (cd->isDefaulted()) {
+			// todo(gmm): i need to figure out how to generate the default constructors
 			output() << "(Some Default)";
 		  } else if (cd->hasBody()) {
 			ctor("Some");
