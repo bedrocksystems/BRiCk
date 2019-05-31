2ee7893272654ca62b7bc1e2ad97ece122385f03
Gregory Malecha <gmalecha@gmail.com>
2019-05-30 23:05:58 -0400

    CLEAN just some minor cleanup

--- theories/Sem/Func.v
+++ theories/Sem/Func.v
@@ -150,44 +150,44 @@ Module Type Func.
   Fixpoint wpi_field (resolve : genv) ti ρ (cls : globname) (this : Loc)
            (ty : type) (f : field) (init : Expr)
            (k : mpred)
-    : mpred :=
-      match ty with
-      | Trv_reference t
-      | Treference t =>
-        (* i should use the type here *)
-        wp_lhs (resolve:=resolve) ti ρ init (fun a free =>
-              (* note(gmm): this is consistent with the specification, but also very strange *)
-              _offsetL (_field f) this &~ a
-           -* (free ** k))
-      | Tfunction _ _ =>
-        (* fields can not be function type *)
-        lfalse
-      | Tvoid => lfalse
-      | Tpointer _
-      | Tbool
-      | Tchar _ _
-      | Tint _ _ =>
-        wp_rhs (resolve:=resolve) ti ρ init (fun v free =>
-           _at (_offsetL (_field f) this) (uninit ty) **
-           (   _at (_offsetL (_field f) this) (tprim ty v)
-            -* (free ** k)))
-      | Tarray _ _ => lfalse (* todo(gmm): arrays not yet supported *)
-      | Tref gn =>
-        match init with
-        | Econstructor cnd es _ =>
-          (* todo(gmm): constructors need to be handled through `cglob`.
-           *)
-          Exists ctor, [| glob_addr resolve cnd ctor |] **
-          (* todo(gmm): is there a better way to get the destructor? *)
-          wps (wpAnys (resolve:=resolve) ti ρ) es (fun vs free =>
-              Forall a, (_offsetL (_field f) this &~ a ** ltrue) //\\
-              |> fspec (Vptr ctor) (a :: vs) ti (fun _ =>
-                 (free ** k))) empSP
-        | _ => lfalse
-          (* ^ all non-primitive declarations must have initializers *)
-        end
-      | Tqualified _ ty => wpi_field resolve ti ρ cls this ty f init k
-      end.
+  : mpred :=
+    match ty with
+    | Trv_reference t
+    | Treference t =>
+      (* i should use the type here *)
+      wp_lhs (resolve:=resolve) ti ρ init (fun a free =>
+            (* note(gmm): this is consistent with the specification, but also very strange *)
+            _offsetL (_field f) this &~ a
+         -* (free ** k))
+    | Tfunction _ _ =>
+      (* fields can not be function type *)
+      lfalse
+    | Tvoid => lfalse
+    | Tpointer _
+    | Tbool
+    | Tchar _ _
+    | Tint _ _ =>
+      wp_rhs (resolve:=resolve) ti ρ init (fun v free =>
+         _at (_offsetL (_field f) this) (uninit ty) **
+         (   _at (_offsetL (_field f) this) (tprim ty v)
+          -* (free ** k)))
+    | Tarray _ _ => lfalse (* todo(gmm): arrays not yet supported *)
+    | Tref gn =>
+      match init with
+      | Econstructor cnd es _ =>
+        (* todo(gmm): constructors need to be handled through `cglob`.
+         *)
+        Exists ctor, [| glob_addr resolve cnd ctor |] **
+        (* todo(gmm): is there a better way to get the destructor? *)
+        wps (wpAnys (resolve:=resolve) ti ρ) es (fun vs free =>
+            Forall a, (_offsetL (_field f) this &~ a ** ltrue) //\\
+            |> fspec (Vptr ctor) (a :: vs) ti (fun _ =>
+               (free ** k))) empSP
+      | _ => lfalse
+        (* ^ all non-primitive declarations must have initializers *)
+      end
+    | Tqualified _ ty => wpi_field resolve ti ρ cls this ty f init k
+    end.
 
   Axiom wpi_field_at : forall resolve ti r this_val x e cls Q,
       wpi_field resolve ti r cls (_eq this_val) (drop_qualifiers (type_of e)) {| f_type := cls ; f_name := x |} e Q
