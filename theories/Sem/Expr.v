14b5249a88fa73d67553e80892c186b032086ee4
John Grosen <jmgrosen@gmail.com>
2019-06-04 16:57:48 -0400

    list proof wip

--- theories/Sem/Expr.v
+++ theories/Sem/Expr.v
@@ -219,6 +219,11 @@ Module Type Expr.
         wp_rhs e (fun v free => [| has_type v t |] ** Q v free)
         |-- wp_rhs (Ecast Cintegral e t) Q.
 
+    Axiom wp_rhs_cast_null : forall e t Q,
+        wp_rhs e Q
+        |-- wp_rhs (Ecast Cnull2ptr e t) Q.
+    (* ^ todo(jmgrosen): confirm that this doesn't change the value *)
+
     (** the ternary operator `_ ? _ : _` *)
     Axiom wp_condition : forall ty m tst th el Q,
         wp_rhs tst (fun v1 free =>
