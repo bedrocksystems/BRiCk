4271eb4d3200b8b062b61ed16fe5bcc0a6f2536a
Gregory Malecha <gmalecha@gmail.com>
2019-06-11 21:19:12 -0400

    experimenting with Travis. (#91)
    * configure travis
    
    * build status
    
    * llvm 8 installation
    
    * default to llvm-config-8
    
    * compile with clang
    
    * try to force it to find llvm-config-8
    
    * install coq-ltac-iter
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update Makefile.common
    
    * Delete Makefile
    
    This should be a link to `Makefile.common`
    
    * Create Makefile
    
    * Update .travis.yml
    
    * Update .travis.yml

--- cmake/modules/FindLLVM.cmake
+++ cmake/modules/FindLLVM.cmake
@@ -1,6 +1,6 @@
 find_program (LLVM_CONFIG
   llvm-config
-  NAMES llvm-config llvm-config-7 llvm-config-8 llvm-config-9
+  NAMES llvm-config-8 llvm-config llvm-config-9 llvm-config-7
   DOC "llvm-config program. if this fails, check that you have the -dev dependencies installed")
 
 execute_process(
