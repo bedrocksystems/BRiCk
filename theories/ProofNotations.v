bfe69dc4bdcd005cf74f1f029778c2ff060fce1f
Gregory Malecha <gmalecha@gmail.com>
2019-08-27 17:28:39 +0000

    use proof notations by default

--- theories/ProofNotations.v
+++ theories/ProofNotations.v
@@ -2,11 +2,31 @@
  *)
 Require Import Cpp.Sem.Wp.
 
-Notation "'wp_rhs' ( e )" := (@wp_rhs _ _ _ e _)
+Notation "'::wpR' ( e )" := (@wp_rhs _ _ _ e _)
          (at level 0, only printing).
-Notation "'wp_lhs' ( e )" := (@wp_lhs _ _ _ e _)
+Notation "'::wpL' ( e )" := (@wp_lhs _ _ _ e _)
          (at level 0, only printing).
-Notation "'wp' ( e )" := (@wp _ _ _ e _)
+Notation "'::wpS' ( e )" := (@wp _ _ _ e _)
          (at level 0, only printing).
-Notation "'wpe' ( e )" := (@wpe _ _ _ _ e _)
+Notation "'::wpE' ( e )" := (@wpe _ _ _ _ e _)
          (at level 0, only printing).
+
+Definition wp_lhs' := @wp_lhs.
+Definition wp_rhs' := @wp_rhs.
+Definition wp' := @wp.
+Definition wpe' := @wpe.
+
+Ltac show :=
+  change @wp_lhs with @wp_lhs';
+  change @wp_rhs with @wp_rhs';
+  change @wpe with @wpe';
+  change @wp with @wp'.
+
+(* note: tactics such as [simplifying] will not work in "visible"
+ * mode
+ *)
+Ltac hide :=
+  change @wp_lhs' with @wp_lhs;
+  change @wp_rhs' with @wp_rhs;
+  change @wpe' with @wpe;
+  change @wp' with @wp.
