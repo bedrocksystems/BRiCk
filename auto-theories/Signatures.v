8fcd140bce25a9b5b013fb3243ea43044ef57f30
Gregory Malecha <gmalecha@gmail.com>
2019-09-19 11:51:15 -0400

    a few revisions to the definitions

--- auto-theories/Signatures.v
+++ auto-theories/Signatures.v
@@ -10,7 +10,31 @@ Definition signature := list (obj_name * function_spec).
 Definition sig {resolve} (ti : thread_info) (s : signature) : mpred :=
   sepSPs (map (fun '(f, fs) => |> cglob (resolve:=resolve) f ti fs) s).
 
-Fixpoint contains (start: nat) (keys: list string) (fullname: string) :bool :=
+Record Matcher : Set :=
+{ matches : string -> bool }.
+
+Local Fixpoint string_rev (acc s : string) : string :=
+  match s with
+  | EmptyString => acc
+  | String s ss => string_rev (String s acc) ss
+  end.
+
+Local Fixpoint namespaces (seen : string) (s : string) : list string :=
+  match s with
+  | String ":" (String ":" s) =>
+    match seen with
+    | EmptyString => namespaces "" s
+    | _ => string_rev "" seen :: namespaces "" s
+    end
+  | String s ss => namespaces (String s seen) ss
+  | EmptyString =>
+    match seen with
+    | EmptyString => nil
+    | _ => string_rev "" seen :: nil
+    end
+  end.
+
+Local Fixpoint contains (start: nat) (keys: list string) (fullname: string) :bool :=
   match keys with
   | kh::ktl =>
     match index 0 kh fullname with
@@ -20,34 +44,48 @@ Fixpoint contains (start: nat) (keys: list string) (fullname: string) :bool :=
   | [] => true
   end.
 
+(* matchers *)
+Definition has (keys : list string) : Matcher :=
+  {| matches := contains 0 keys |}.
+
+Definition name (str : string) : Matcher :=
+  has (namespaces "" str).
+
+Definition exact (s : string) : Matcher :=
+  {| matches := String.eqb s |}.
+
 Definition extMethod (o: ObjValue) : option Method :=
   match o with
   | Omethod m => Some m
   | _ => None
-  end.    
+  end.
 
 Definition extFunc (o: ObjValue) : option Func :=
   match o with
   | Ofunction m => Some m
   | _ => None
-  end.    
+  end.
 
 Definition extCtor (o: ObjValue) : option Ctor :=
   match o with
   | Oconstructor m => Some m
   | _ => None
-  end.    
+  end.
 
-Definition extItem {I} (ext: ObjValue -> option I) (matchName: string-> bool) (c: compilation_unit): (string*I) + string :=
-  match (List.filter (fun '(n, b) =>
-                       ssrbool.isSome (ext b) && matchName n)%bool
-                     (symbols c)) with
-  | [] => inr "not found"
-  | h::[] => match ext (snd h) with
-           | Some m => inl (fst h, m)
-           | _ => inr "not possible"
-           end
-  | _::_::_ => inr "multiple matches"
+Definition extItem {I} (ext: ObjValue -> option I) (matchName: Matcher)
+           (c: compilation_unit): (string*I) + string :=
+  let result :=
+      List.flat_map (fun '(n, b) =>
+                       match ext b with
+                       | None => nil
+                       | Some m => if matchName.(matches) n then (n, m) :: nil else nil
+                       end)
+                    (symbols c)
+  in
+  match result with
+  | [] => inr "found no matching symbols"
+  | h::[] => inl h
+  | _::_::_ => inr ("Ambiguous match. The following symbols matched: " ++ String.concat ", " (List.map fst result))
   end.
 
 Definition SMethodSpec (msig: Method)
@@ -59,7 +97,7 @@ Definition SMethodSpec (msig: Method)
 
 Definition SFunctionSpec (msig: Func)
            (PQ : arrowFrom val (map snd (f_params msig)) WithPrePost) :=
-  SFunction 
+  SFunction
           (f_return msig)
           (map snd (f_params msig)) PQ.
 
@@ -72,14 +110,14 @@ Definition SCtorSpec (msig: Ctor)
 Ltac specItem specFun ext nameMatch module spec :=
   let t := eval hnf in (extItem ext nameMatch module) in
   let t := eval simpl in t in
-      match t with
-      | inr ?x => idtac x; exact x
-      | inl ?x => exact (specFun (snd x) spec)
-      end.
+  lazymatch t with
+  | inr ?x => fail 1 x
+  | inl ?x => exact (specFun (snd x) spec)
+  end.
 
-Definition has : list string -> string  -> bool := contains 0.
+
+
 Ltac specMethod  := specItem SMethodSpec extMethod.
 (* it is hard to identify constructors by name*)
 Ltac specCtor  := specItem SCtorSpec extCtor.
 Ltac specFunc  := specItem SFunctionSpec extFunc.
-
