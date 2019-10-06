aa0172176f1a284aec342d1b27e8f6a6bbf208ef
Abhishek Anand <abhishek.anand.iitg@gmail.com>
2019-10-06 02:26:14 +0000

    directives for hiding gFunctors argument behind typeclass resolution

---
+++ theories/GFunClass.v
@@ -0,0 +1,9 @@
+Require Import Cpp.Auto.
+Existing Class gFunctors.
+
+Arguments mpred {_}.
+Arguments Rep {_}.
+Arguments Offset {_}.
+Arguments Signature.signature {_}.
+Arguments function_spec {_}.
+
