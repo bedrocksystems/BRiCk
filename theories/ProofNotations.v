cd597f234f12eda1791a4a1ab73e4f5d6fdc6916
Gregory Malecha <gmalecha@gmail.com>
2019-10-15 18:45:34 +0000

    make it possible to disable proof notations

--- theories/ProofNotations.v
+++ theories/ProofNotations.v
@@ -3,25 +3,49 @@
 Require Import Cpp.Sem.Wp.
 Require Import Cpp.Sem.
 
-Notation "'::wpL' ( e )" := (@wp_lval _ _ _ _ e _)
+Module Compact.
+Notation "'::wpL' { e }" := (@wp_lval _ _ _ _ e _)
          (at level 0, only printing).
-Notation "'::wpX' ( e )" := (@wp_xval _ _ _ _ e _)
+Notation "'::wpX' { e }" := (@wp_xval _ _ _ _ e _)
          (at level 0, only printing).
-Notation "'::wpPR' ( e )" := (@wp_prval _ _ _ _ e _)
+Notation "'::wpPR' { e }" := (@wp_prval _ _ _ _ e _)
          (at level 0, only printing).
-Notation "'::wpPRᵢ' ( e )" := (@wp_init _ _ _ _ _ _ e _)
+Notation "'::wpPRᵢ' { e }" := (@wp_init _ _ _ _ _ _ e _)
          (at level 0, only printing).
-Notation "'::wpGL' ( e )" := (@wp_glval _ _ _ _ e _)
+Notation "'::wpGL' { e }" := (@wp_glval _ _ _ _ e _)
          (at level 0, only printing).
-Notation "'::wpR' ( e )" := (@wp_rval _ _ _ _ e _)
+Notation "'::wpR' { e }" := (@wp_rval _ _ _ _ e _)
          (at level 0, only printing).
-Notation "'::wpS' ( e )" := (@wp _ _ _ _ e _)
+Notation "'::wpS' { e }" := (@wp _ _ _ _ e _)
          (at level 0, only printing).
-Notation "'::wpE' ( e )" := (@wpe _ _ _ _ _ e _)
+Notation "'::wpE' { e }" := (@wpe _ _ _ _ _ e _)
          (at level 0, only printing).
-Notation "'::wpD' ( t n = e )" := (@wp_decl _ _ _ n t e _ _ _)
+Notation "'::wpD' { t n = e }" := (@wp_decl _ _ _ n t e _ _ _)
          (at level 0, only printing).
+End Compact.
 
+Module Verbose.
+Notation "'::wpL' { e } Q" := (@wp_lval _ _ _ _ e Q)
+         (at level 0, Q at level 200, only printing).
+Notation "'::wpX' { e } Q" := (@wp_xval _ _ _ _ e Q)
+         (at level 0, Q at level 200, only printing).
+Notation "'::wpPR' { e } Q" := (@wp_prval _ _ _ _ e Q)
+         (at level 0, Q at level 200, only printing).
+Notation "'::wpPRᵢ' { e } Q" := (@wp_init _ _ _ _ _ _ e Q)
+         (at level 0, Q at level 200, only printing).
+Notation "'::wpGL' { e } Q" := (@wp_glval _ _ _ _ e Q)
+         (at level 0, Q at level 200, only printing).
+Notation "'::wpR' { e } Q" := (@wp_rval _ _ _ _ e Q)
+         (at level 0, Q at level 200, only printing).
+Notation "'::wpS' { e } Q" := (@wp _ _ _ _ e Q)
+         (at level 0, Q at level 200, only printing).
+Notation "'::wpE' { e } Q" := (@wpe _ _ _ _ _ e Q)
+         (at level 0, Q at level 200, only printing).
+Notation "'::wpD' { t n = e } Q" := (@wp_decl _ _ _ n t e _ _ Q)
+         (at level 0, Q at level 200, only printing).
+End Verbose.
+
+(*
 Definition wp_lval' := @wp_lval.
 Definition wp_prval' := @wp_prval.
 Definition wp_xval' := @wp_xval.
@@ -56,3 +80,4 @@ Ltac hide :=
   change @wp_decl' with @wp_decl;
   change @wpe' with @wpe;
   change @wp' with @wp.
+*)
