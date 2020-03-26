30dc9f7358f20b7f0ef0ab7d913ee11e37fe4c35
Abhishek Anand (optiplex7010@home) <abhishek.anand.iitg@gmail.com>
2020-03-26 13:59:15 -0700

    back to tmFail

--- theories/TC/TC.v
+++ theories/TC/TC.v
@@ -76,7 +76,7 @@ Local Definition getFields (mi : mutual_inductive_body) (n : nat)
       | nil =>
         let ctor_arity := get_arity ctor_type in
         if decide (ctor_arity > get_arity oib.(ind_type)) then
-          print_nf ("info: the constructor " ++ ctor_name ++ " has no projections but an arity of " ++ (MCString.string_of_nat ctor_arity) ++ ". Perhaps you forgot to enable primitive projections before the definition of the Inductive")
+          tmFail ("info: the constructor " ++ ctor_name ++ " has no projections but an arity of " ++ (MCString.string_of_nat ctor_arity) ++ ". Perhaps you forgot to enable primitive projections before the definition of the Inductive")
         else ret tt
       | _ => ret tt
       end ;;
