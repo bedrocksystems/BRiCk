79d323f169bc89b3e3bc4bc3f28e6dc896e4da43
Gregory Malecha <gmalecha@gmail.com>
2020-05-08 09:15:08 -0400

    CLEAN syntactic simplification in call

--- theories/lang/cpp/logic/call.v
+++ theories/lang/cpp/logic/call.v
@@ -31,13 +31,13 @@ Section with_resolve.
                                    Qarg v free -* Q (v :: vs) (free ** frees))
       | Rvalue =>
         if is_aggregate ty then
-          Forall a, _at (_eqv a) (uninitR (resolve:=σ) (erase_qualifiers ty) 1) -*
+          Forall a, _at (_eq a) (uninitR (resolve:=σ) (erase_qualifiers ty) 1) -*
           let (e,dt) := destructor_for e in
           Exists Qarg,
-          wp_init ty a e Qarg **
+          wp_init ty (Vptr a) e Qarg **
             wp_args es (fun vs frees =>
                           Forall free,
-                          Qarg free -* Q (a :: vs) (mdestroy (σ:=σ) ti ty a dt free ** frees))
+                          Qarg free -* Q (Vptr a :: vs) (mdestroy (σ:=σ) ti ty (Vptr a) dt free ** frees))
         else
           Exists Qarg,
           wp_prval e Qarg **
