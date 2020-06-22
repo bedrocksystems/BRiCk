5e967c1a1aa24e37a2b4c82007251a50c0a453fe
Gregory Malecha <gmalecha@gmail.com>
2020-06-22 20:58:36 +0000

    BUG destructors to fields should be called on the field.

--- theories/lang/cpp/logic/destructors.v
+++ theories/lang/cpp/logic/destructors.v
@@ -1,5 +1,5 @@
 (*
- * Copyright (C) BedRock Systems Inc. 2019 Gregory Malecha
+ * Copyright (C) BedRock Systems Inc. 2019-2020 Gregory Malecha
  *
  * SPDX-License-Identifier: LGPL-2.1 WITH BedRock Exception for use over network, see repository root for details.
  *)
@@ -7,17 +7,16 @@ Require Import Coq.Lists.List.
 
 From bedrock.lang.cpp Require Import ast semantics.
 From bedrock.lang.cpp.logic Require Import
-     pred path_pred heap_pred wp.
+     pred path_pred heap_pred wp destroy.
 
 Section with_resolve.
   Context `{Σ : cpp_logic thread_info} {σ: genv}.
   Variables (M : coPset) (ti : thread_info) (ρ : region).
 
   Axiom wpd_deinit : forall (cls : globname) (this : val) path dn (Q : epred),
-    Exists dp, Exists fp,
-      (_global (resolve:=σ) dn &~ dp **
-       _offsetL (offset_for σ cls path) (_eqv this) &~ fp ** ltrue) //\\
-      (|> fspec ti (Vptr dp) (this :: nil) (fun _ => Q))
+    Exists fp,
+      (_offsetL (offset_for σ cls path) (_eqv this) &~ fp ** ltrue) //\\
+      (destruct (σ:=σ) ti (Tnamed cls) (Vptr fp) dn Q)
     |-- wpd (resolve:=σ) M ti ρ cls this (path, dn) Q.
 
 End with_resolve.
