d44fb9e352fc0e5d4c913b4086975ee0435c9e68
Jasper Haag <jasperhaag16@gmail.com>
2020-09-14 15:53:14 -0400

    Update definition of 'layout#decodes' to properly account for a user-specified endianness

--- theories/lang/cpp/logic/layout.v
+++ theories/lang/cpp/logic/layout.v
@@ -127,11 +127,11 @@ Section with_Σ.
   Local Notation Unfold x tm :=
     ltac:(let H := eval unfold x in tm in exact H) (only parsing).
 
-  Definition decodes (sgn: signed) (l: list N) (z: Z) :=
-    _Z_from_bytes (values.byte_order resolve) sgn l = z.
+  Definition decodes (endianness: endian) (sgn: signed) (l: list N) (z: Z) :=
+    _Z_from_bytes endianness sgn l = z.
 
   Definition decodes_uint (l : list N) (z : Z) :=
-    Unfold decodes (decodes Unsigned l z).
+    Unfold decodes (decodes (values.byte_order resolve) Unsigned l z).
 
   (* JH: TODO: Determine what new axioms we should add here. *)
   Axiom raw_byte_of_int_eq : forall sz x rs,
