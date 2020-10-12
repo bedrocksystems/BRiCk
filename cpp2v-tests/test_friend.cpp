2fc77e07943825f18d465bc04ab22c444a1582ff
Gregory Malecha <gmalecha@gmail.com>
2020-10-12 11:35:00 -0400

    tests for friend declarations

--- cpp2v-tests/test_friend.cpp
+++ cpp2v-tests/test_friend.cpp
@@ -2,5 +2,13 @@ class X {
   friend int get(X&x) {
     return x.x;
   }
+  friend int blab(X&);
+  friend int blab2(X&);
   int x;
 };
+
+int blab(X& x) { return x.x; }
+int test() {
+  X x;
+  return blab2(x);
+}
