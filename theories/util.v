f3b1631caeec24e3dd0f433a734113357cbde025
Abhishek Anand (optiplex) <abhishek.anand.iitg@gmail.com>
2020-10-15 10:57:37 -0700

    addressed more comments

--- theories/util.v
+++ theories/util.v
@@ -10,13 +10,13 @@ Section lift.
   Fixpoint lift_aux n (pa : paddr) (q:Qp) (v : N) :=
     match n with
     | O => byte_at pa q v
-    | S n => let bytesize :N := (2^(N.of_nat n)) in
-            let bitsize := (8*bytesize)%N in
-            lift_aux n pa q (N.land v (2^bitsize - 1))
+    | S n => let bytesize :N := (2 ^ (N.of_nat n)) in
+            let bitsize := (8 * bytesize)%N in
+            lift_aux n pa q (N.land v (2 ^ bitsize - 1))
             ** lift_aux n (pa+bytesize) q (N.shiftr v bitsize)
     end.
 
-  Definition lift (nbytes:N) (pa : paddr) (q:Qp) (v : N) :=
+  Definition bytes_at (nbytes:N) (pa : paddr) (q:Qp) (v : N) :=
     lift_aux (N.to_nat (BinNatDef.N.log2 nbytes)) pa q v.
 
   Section test.
@@ -29,11 +29,11 @@ Section lift.
       short_at pa q (N.land v (2^16 - 1))
       ** short_at (pa + 2)%N q (N.shiftr v 16).
 
-    Example t1 pa q v : short_at pa q v = lift 2 pa q v.
+    Example t1 pa q v : short_at pa q v = bytes_at 2 pa q v.
     Proof.
       reflexivity.
     Abort.
-    Example t2 pa q v : word_at pa q v = lift 4 pa q v.
+    Example t2 pa q v : word_at pa q v = bytes_at 4 pa q v.
     Proof.
       reflexivity.
     Abort.
