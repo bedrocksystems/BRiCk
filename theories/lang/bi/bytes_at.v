1570a5bbf1ba23315dd648e40a3f28ee5a16dafc
Abhishek Anand (optiplex) <abhishek.anand.iitg@gmail.com>
2020-10-15 20:01:31 -0700

    addressed other comments

--- theories/lang/bi/bytes_at.v
+++ theories/lang/bi/bytes_at.v
@@ -1,28 +1,29 @@
 From iris Require Import bi.bi.
 From bedrock Require Import IrisBridge.
 Import ChargeNotation.
-Open Scope N_scope.
+Local Open Scope N_scope.
 Section lift.
   Context {PROP: bi}.
   Notation paddr := N.
   Variable byte_at : forall (hpa:paddr) (hpaFrac : Qp) (value : N), PROP.
 
-  Fixpoint lift_aux n (pa : paddr) (q:Qp) (v : N) :=
+  Local Fixpoint lift_aux n (pa : paddr) (q:Qp) (v : N) :=
     match n with
     | O => byte_at pa q v
     | S n => let bytesize :N := (2 ^ (N.of_nat n)) in
-            let bitsize := (8 * bytesize)%N in
+            let bitsize := 8 * bytesize in
             lift_aux n pa q (N.land v (2 ^ bitsize - 1))
             ** lift_aux n (pa+bytesize) q (N.shiftr v bitsize)
     end.
 
+  (* byte by byte, layout the bits of [v] starting at [paddr], msb first, using the [byte_at] parameter to lay out each byte *)
   Definition bytes_at (nbytes:N) (pa : paddr) (q:Qp) (v : N) :=
     lift_aux (N.to_nat (BinNatDef.N.log2 nbytes)) pa q v.
 
   Section test.
 
     Let short_at (pa : paddr) (q:Qp) (v : N) : PROP :=
-      byte_at pa q (N.land v (2^8 - 1))%N
+      byte_at pa q (N.land v (2^8 - 1))
       ** byte_at (pa + 1)%N q (N.shiftr v 8).
 
     Let word_at (pa : paddr) (q : Qp) (v : N) : PROP :=
