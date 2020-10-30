4e91c40bcb4bb5d2c56b391fc54565bf15d2aec1
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-10-30 03:33:55 +0100

    Add stdpp-style sections of new `==` operator

--- theories/lang/prelude/notations.v
+++ theories/lang/prelude/notations.v
@@ -8,3 +8,13 @@
 From stdpp Require Import base.
 
 Infix "==" := (≡) (at level 70, no associativity, only parsing) : stdpp_scope.
+
+Infix "==@{ A }" := (≡@{A})
+  (at level 70, only parsing, no associativity) : stdpp_scope.
+
+Notation "(==)" := (≡) (only parsing) : stdpp_scope.
+Notation "( X ==.)" := ( X ≡.) (only parsing) : stdpp_scope.
+Notation "(.== X )" := (.≡ X ) (only parsing) : stdpp_scope.
+Notation "(==@{ A } )" := (≡@{ A } ) (only parsing) : stdpp_scope.
+
+(** For now, no ASCII notation for [≢]. *)
