2b2600b419159abc31d4b119dcb0d8bcbeacf470
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-11-17 18:23:11 +0100

    Move o_sub_0 back to pred.v
    We need size_of here.

--- theories/lang/cpp/semantics/values.v
+++ theories/lang/cpp/semantics/values.v
@@ -126,10 +126,6 @@ Module Type PTRS.
   (* [o_sub ty n] represents [x + n] for [x : cls*] *)
   Parameter o_sub : genv -> type -> Z -> offset.
 
-  Axiom o_sub_0 : ∀ σ ty,
-    size_of σ ty = Some n ->
-    o_sub σ ty 0 = o_id.
-
   (** going up and down the class hierarchy, one step at a time. *)
   Parameter o_base : genv -> forall (derived base : globname), offset.
   Parameter o_derived : genv -> forall (base derived : globname), offset.
