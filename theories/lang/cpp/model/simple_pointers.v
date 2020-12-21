a1e4bede3c207ec416606da57985c56323124e21
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-12-21 03:14:58 +0100

    Drop new dead code

--- theories/lang/cpp/model/simple_pointers.v
+++ theories/lang/cpp/model/simple_pointers.v
@@ -460,25 +460,9 @@ Module PTRS_IMPL : PTRS_INTF.
     | _ => True
     end.
   Proof.
-    (* inversion 1 => //; case_match; simplify_eq/=; try naive_solver. *)
     move E: (o_sub_ _ _ :: _) => d' Hcn.
     elim: Hcn z E; naive_solver.
   Qed.
-
-  Lemma foo_o_base_cancel_wf s d derived base :
-    roff_canon (o_derived_ base derived :: s) (o_derived_ base derived :: d) ->
-    roff_canon s d.
-  Proof. intros H.
-  (* dependent induction H; eauto. *)
-    inversion H; subst; eauto using roff_canon_o_base_inv.
-    Fail Qed.
-    Abort.
-
-  (* Lemma foo_o_derived_cancel_wf' o s d :
-    roff_canon (o :: s) (o :: d) ->
-    roff_canon s d.
-  Proof. inversion 1; subst; eauto using foo_o_derived_cancel_wf. apply foo_o_derived_cancel_wf in H3. Qed. *)
-
 
   Definition offset_seg_merge (os1 os2 : offset_seg) : list offset_seg :=
     match os1, os2 with
@@ -515,44 +499,6 @@ Module PTRS_IMPL : PTRS_INTF.
     by efeed pose proof roff_canon_o_sub_wf.
   Qed.
 
-  (* Theorem canon_wf_keep o src dst :
-    roff_canon (o :: src) (o :: dst) ->
-    roff_canon src dst.
-  Proof.
-    move E1: (_ :: src) => s1.
-    move E2: (_ :: dst) => s2 Hrc.
-    move: Hrc o src dst E1 E2.
-    induction 1; intros; simplify_eq/=; eauto.
-    - *)
-    (* destruct dst => //.
-    admit.
-    econstructor. *)
-    (* rewrite Z.add_0_l.
-    clear H4. have {H2} Hz1 : 0 < z1 by lia.
-    constructor => //.
-    destruct d. admit.
-    destruct d.
-    econstructor.
-    rewrite left_id_L.
-    eauto.
-    inversion H6; simplify_eq/=; try naive_solver.
-    inversion Hrc; simplify_eq/=; try naive_solver.
-    destruct d. admit.
-    inversion Hfoo; simplify_eq/=; try naive_solver.
-    econstructor => //. *)
-
-  (* Lemma foo ty z1 dst r rs (Hgt : 0 < z1) :
-    raw_offset_wf (zip (o_sub_ ty z1 :: dst) (r :: rs)) ->
-    raw_offset_wf (zip dst rs).
-  Proof.
-    rewrite /raw_offset_wf /raw_offset_collapse.
-      rewrite /merge_elem /=.
-      case: merge_elems => [|o os] IH; simplify_eq/=. by destruct zip.
-
-      destruct o as [[] ?]; simplify_eq/=; try by rewrite -IH.
-      case_decide; simplify_eq/=; repeat (lia || f_equal || done).
-  Admitted. *)
-
   Theorem canon_wf src dst rs : roff_canon src dst -> raw_offset_wf (zip dst rs).
   Proof.
     rewrite /raw_offset_wf /raw_offset_collapse => /canon_wf_0 Hc.
@@ -570,76 +516,6 @@ Module PTRS_IMPL : PTRS_INTF.
       destruct o as [[] ?]; simplify_eq/=; try by rewrite -IH.
       case_decide; simplify_eq/=; repeat (lia || f_equal). done.
   Qed.
-(*
-    move: IHHc => /(_ (r :: rs)) /foo.
-    rewrite /raw_offset_wf /raw_offset_collapse.
-
-      rewrite /merge_elem /= => IH. rewrite -IH. move: IH.
-      case: merge_elems => //=.
-      => [|o os] IH; simplify_eq/=. by destruct zip.
-
-      destruct o as [[] ?]; simplify_eq/=; try by rewrite -IH.
-      case_decide; simplify_eq/=; repeat (lia || f_equal). done.
-
-
-        rewrite H0.
-        f_equiv.
-        lia.
-        repeat f_equiv => //.
-        repeat case_match; simplify_eq/=.
-        destruct o.
-        congruence.
-        destruct zs, d => //=.
-      repeat case_match.
-      inversion Hc; subst.
-      +  case_match => //. destruct zs => //=.
-        case_match => //=. by rewrite decide_False.
-      destruct d, zs; simplify_eq/=.
-        case_match.
-      admit.
-      {
-        rewrite /merge_elem. IHHc.
-        destruct d as [|[] ds] => //=; eauto.
-        case: zs d IHhc Hc H => //= [|z1 zs] [|? d].
-      by rewrite /merge_elem /offset_seg_merge /=; repeat case_match. }
-      rewrite /merge_elem /=.
-      case E: merge_elems => [|os oss]; simplify_eq.
-      -
-      (* destruct d as [|? [|? d]], zs as [|? [|? zs]]; simplify_eq/= =>//. *)
-      admit.
-      -
-      simpl in IHHc.
-      rewrite /offset_seg_merge; simplify_eq/=.
-      specialize
-      destruct d as [|d0 d], zs as [|z zs] => //=.
-      repeat case_match => //; subst; simplify_eq/=.
-      rewrite /= -E. *)
-
-    (* elim: dst src Hc zs => [//|d dst IHdst] src Hc [//|z zs] /=.
-    inversion Hc; subst => //; rewrite (IHdst s) //.
-      all: try by rewrite /merge_elem /offset_seg_merge /=; repeat case_match. *)
-
-    (* - rewrite (IHdst s);
-      by rewrite /merge_elem /offset_seg_merge /=; repeat case_match.
-    -
-      rewrite (IHdst s) //.
-        by rewrite /merge_elem /offset_seg_merge /=; repeat case_match.
-    -
-    inversion H3; subst => //.
-      + rewrite (IHdst s0) //.
-        by rewrite /merge_elem /offset_seg_merge /=; repeat case_match.
-      +
-        rewrite (IHdst s0) //.
-      econstructor.
-      by .
-
-    induction 1 => -[//|z zs] /=. {
-      rewrite IHHc /=.
-      rewrite /merge_elem /= /offset_seg_merge.
-      repeat case_match => //.
-    }
-    rewrite IHHc /=. *)
-
 
   Definition raw_offset_merge (o1 o2 : raw_offset) : raw_offset :=
     raw_offset_collapse (o1 ++ o2).
