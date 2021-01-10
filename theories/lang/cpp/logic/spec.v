08e4aa4891a6d0b60737d873e47ee8b406b50b4a
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2021-01-10 20:50:55 +0100

    spec.v: generalize and revise
    - Generalize WithEx/WppD (as done downstream)
    - Rename general version of WppD to WppDG
    - Add specialized adapters with existing interface
    - Drop tbi_exist/tbi_forall and their uses
    - Use `assert` for `Arguments` for extra checking.

--- theories/lang/cpp/logic/spec.v
+++ theories/lang/cpp/logic/spec.v
@@ -17,54 +17,51 @@ Local Set Universe Polymorphism.
 Section with_prop.
   Context {PROP : bi}.
 
-  Record WithEx@{X Z Y} : Type :=
+  Record WithExG@{X Z Y R} {RESULT : Type@{R}} : Type :=
     { we_ex   : tele@{X}
-    ; we_post : tele_fun@{X Z Y} we_ex (val * PROP) }.
+    ; we_post : tele_fun@{X Z Y} we_ex (RESULT * PROP) }.
+  Global Arguments WithExG : clear implicits.
 
-  Definition WithEx_map (f : val -> PROP -> val * PROP) (we : WithEx)
-    : WithEx :=
+  Definition WithExG_map@{X Z Y T U} {T : Type@{T}} {U : Type@{U}} (f : T -> PROP -> U * PROP)
+    (we : WithExG@{X Z Y T} T) : WithExG@{X Z Y U} U :=
     {| we_ex := we.(we_ex)
      ; we_post := tele_map (fun '(r,Q) => f r Q) we.(we_post) |}.
 
-  Record WithPrePost@{X Z Y} : Type :=
+  Record WithPrePostG@{X Z Y A R} {ARGS : Type@{A}} {RESULT : Type@{R}} :=
     { wpp_with : tele@{X}
-    ; wpp_pre  : tele_fun@{X Z Y} wpp_with (list val * PROP)
-    ; wpp_post : tele_fun@{X Z Y} wpp_with WithEx@{X Z Z} }.
+    ; wpp_pre  : tele_fun@{X Z Y} wpp_with (ARGS * PROP)
+    ; wpp_post : tele_fun@{X Z Y} wpp_with (WithExG@{X Z _ _} RESULT)}.
+  Global Arguments WithPrePostG : clear implicits.
 
-  Fixpoint tbi_exist@{X Z Y} {t : tele@{X}}
-    : forall (P : tele_fun@{X Z Y} t PROP), PROP :=
-    match t as t0 return ((t0 -t> PROP) → PROP) with
-    | [tele] => fun x : PROP => x
-    | @TeleS X binder =>
-      fun P : (∀ x : X, binder x -t> PROP) => Exists x : X, tbi_exist (P x)
-    end.
-
-  Fixpoint tbi_forall@{X Z Y} {t : tele@{X}}
-    : forall (P : tele_fun@{X Z Y} t PROP), PROP :=
-    match t as t0 return ((t0 -t> PROP) → PROP) with
-    | [tele] => fun x : PROP => x
-    | @TeleS X binder =>
-      fun P : (∀ x : X, binder x -t> PROP) => Forall x : X, tbi_forall (P x)
-    end.
-
-  Definition WppD@{X Z Y} (wpp : WithPrePost@{X Z Y}) (params : list val)
-             (Q : val -> PROP) : PROP :=
-    tbi_exist@{X Z Y} (tele_bind (TT:=wpp.(wpp_with)) (fun args =>
+  (** Mnemonic: WppGD stands for "[WithPrePostG]'s denotation" *)
+  Definition WppGD@{X Z Y A R} {ARGS RESULT} (wpp : WithPrePostG@{X Z Y A R} ARGS RESULT) (params : ARGS)
+             (Q : RESULT -> PROP) : PROP :=
+    bi_texist (fun args =>
       let P := tele_app wpp.(wpp_pre) args in
       let Q' := tele_app wpp.(wpp_post) args in
       [| params = fst P |] ** snd P **
-      tbi_forall@{X Z Y} (tele_map (fun '(result,Q') => Q' -* Q result) Q'.(we_post)))).
+      bi_tforall (tele_app@{_ X _} (tele_map (fun '(result,Q') => Q' -* Q result) Q'.(we_post)))).
 
+  (* Aliases specialized to [val]. We use definitions to control the universe
+  arguments. *)
+  Definition WithEx@{X Z Y} := WithExG@{X Z Y _} val.
+  Definition WithPrePost@{X Z Y} := WithPrePostG@{X Z Y _ _} (list val) val.
+  Definition WppD@{X Z Y} := (WppGD@{X Z Y _ _} (ARGS := list val) (RESULT := val)).
+  Definition WithEx_map@{X Z Y} := WithExG_map@{X Z Y _ _} (T := val) (U := val).
 End with_prop.
+
+Arguments WithPrePostG : clear implicits.
 Arguments WithPrePost : clear implicits.
-Arguments Build_WithPrePost _ _ _ : clear implicits.
+Arguments Build_WithPrePostG _ _ _ : clear implicits.
+Arguments WithExG : clear implicits.
 Arguments WithEx : clear implicits.
-Arguments Build_WithEx _ _ _ : clear implicits.
+Arguments Build_WithExG _ _ _ : clear implicits.
 
-Arguments we_ex {PROP} _.
-Arguments we_post {PROP} _.
-Arguments wpp_with {_} _.
-Arguments wpp_pre {_} _.
-Arguments wpp_post {_} _.
+Arguments we_ex {PROP RESULT} _ : assert.
+Arguments we_post {PROP RESULT} _ : assert.
+Arguments wpp_with {PROP ARGS RESULT} _: assert.
+Arguments wpp_pre {PROP ARGS RESULT} _: assert.
+Arguments wpp_post {PROP ARGS RESULT} _: assert.
 
-Global Arguments WppD _ !_ _ _ /.
+Global Arguments WppGD {PROP ARGS RESULT} !wpp _ _ / : assert.
+Global Arguments WppD {PROP} !wpp _ _ / : assert.
