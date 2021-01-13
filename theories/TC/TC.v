83ba8023df323be7087a69b355309c351ab379f7
Jan-Oliver Kaiser <janno@bedrocksystems.com>
2021-01-13 18:27:01 +0100

    Make code compile with coq 8.13.

--- theories/TC/TC.v
+++ theories/TC/TC.v
@@ -50,8 +50,8 @@ Local Definition mkLens (At : term) (fields : list (ident * term)) (i : nat)
       Some ( lensName "_" name
            , tApp cBuild_Lens
                   (At :: At :: Bt :: Bt ::
-                      tLambda nAnon At get_body ::
-                      tLambda nAnon (tProd nAnon Bt Bt) (tLambda nAnon At update_body) ::
+                      tLambda (mkBindAnn nAnon Irrelevant) At get_body ::
+                      tLambda (mkBindAnn nAnon Irrelevant) (tProd (mkBindAnn nAnon Irrelevant) Bt Bt) (tLambda (mkBindAnn nAnon Irrelevant) At update_body) ::
                       nil))
     end
   | _ => None
