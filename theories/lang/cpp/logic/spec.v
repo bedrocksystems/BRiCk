264dcffddbdf3e7c6bdec16d94f7c40c3466d6e5
David Swasey <david@bedrocksystems.com>
2021-06-28 10:01:33 +0200

    Move tele_fun OFE to bedrock.lang.algebra.telescopes.

--- theories/lang/cpp/logic/spec.v
+++ theories/lang/cpp/logic/spec.v
@@ -1,16 +1,12 @@
 (*
- * Copyright (c) 2020 BedRock Systems, Inc.
+ * Copyright (c) 2020-21 BedRock Systems, Inc.
  * This software is distributed under the terms of the BedRock Open-Source License.
  * See the LICENSE-BedRock file in the repository root for details.
  *)
-Require Import Coq.Lists.List.
-
-Require Import stdpp.telescopes.
-
-From iris.proofmode Require Import tactics.
-
-From bedrock.lang.cpp Require Import ast semantics.
-From bedrock.lang.cpp Require Import pred.
+Require Import bedrock.lang.algebra.telescopes.
+Require Import bedrock.lang.bi.telescopes.
+Require Import bedrock.lang.cpp.semantics.values.
+Import ChargeNotation.
 
 Local Set Universe Polymorphism.
 
@@ -83,25 +79,6 @@ Arguments wpp_post {PROP ARGS RESULT} _: assert.
 
 Global Arguments WppGD {PROP ARGS RESULT} !wpp _ _ / : assert.
 Global Arguments WppD {PROP} !wpp _ _ / : assert.
-
-(** Support for guarded recursive specs. *)
-Section tele_fun_ofe.
-  Context {t : tele} {A : ofeT}.
-
-  (** Imposing a discrete order here might be limiting in practice,
-      but the same limitation exists upstream; for example, in
-      [bi_texist_ne]. *)
-  Instance tele_fun_equiv : Equiv (t -t> A) :=
-    fun f g => forall x, tele_app f x ≡ tele_app g x.
-  Instance tele_fun_dist : Dist (t -t> A) :=
-    fun n f g => forall x, tele_app f x ≡{n}≡ tele_app g x.
-
-  Lemma tele_fun_ofe_mixin : OfeMixin (t -t> A).
-  Proof. exact: (iso_ofe_mixin (A:=tele_arg t -d> A) tele_app). Qed.
-
-  Canonical Structure tele_funO := OfeT (t -t> A) tele_fun_ofe_mixin.
-End tele_fun_ofe.
-Arguments tele_funO _ _ : clear implicits, assert.
 
 Section tele_fun_quantifiers.
   Context {PROP : bi} {t : tele}.
