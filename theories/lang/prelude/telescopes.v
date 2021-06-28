419e26717cbc0fb61f11c1a14715e2718f5aba74
David Swasey <david@bedrocksystems.com>
2021-06-28 10:35:38 +0200

    Make tbi_exist_exist, tbi_forall_forall universe polymorphic.

--- theories/lang/prelude/telescopes.v
+++ theories/lang/prelude/telescopes.v
@@ -6,10 +6,14 @@
 Require Export stdpp.telescopes.
 Require Export bedrock.lang.prelude.base.
 
-Set Universe Polymorphism.
+#[local] Set Universe Polymorphism.
 
 Fixpoint tele_append (t : tele) {struct t}: (t -t> tele) -> tele :=
   match t as t return (t -t> tele) -> tele with
   | TeleO => fun x : tele => x
   | @TeleS T f => fun x => @TeleS T (fun t => tele_append (f t) (x t))
   end.
+
+Definition tele_fun_pointwise@{X Z Y} {t : tele@{X}} {A : Type@{Z}}
+    (R : A -> A -> Prop) (f g : tele_fun@{X Z Y} t A) : Prop :=
+  forall x, R (tele_app f x) (tele_app g x).
