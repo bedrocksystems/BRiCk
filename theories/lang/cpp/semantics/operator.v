4b36226982b4303f67c3bfbdc923eb6906b07707
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2021-07-17 19:30:00 +0200

    semantics.operator: revise

--- theories/lang/cpp/semantics/operator.v
+++ theories/lang/cpp/semantics/operator.v
@@ -68,7 +68,7 @@ Axiom eval_minus_int : forall resolve (s : signed) a c w,
     eval_unop (resolve:=resolve) Uminus (Tint w s) (Tint w s)
               (Vint a) (Vint c).
 
-Definition eval_int_op (bo : BinOp) (o : Z -> Z -> Z) : Prop :=
+#[local] Definition eval_int_op (bo : BinOp) (o : Z -> Z -> Z) : Prop :=
   forall resolve w (s : signed) (a b c : Z),
     has_type (Vint a) (Tint w s) ->
     has_type (Vint b) (Tint w s) ->
@@ -80,7 +80,7 @@ Definition eval_int_op (bo : BinOp) (o : Z -> Z -> Z) : Prop :=
     eval_binop_pure (resolve:=resolve) bo (Tint w s) (Tint w s) (Tint w s) (Vint a) (Vint b) (Vint c).
 
 (* this is bitwise operators *)
-Definition eval_int_bitwise_op (bo : BinOp) (o : Z -> Z -> Z) : Prop :=
+#[local] Definition eval_int_bitwise_op (bo : BinOp) (o : Z -> Z -> Z) : Prop :=
   forall resolve w (s : signed) (a b c : Z),
     has_type (Vint a) (Tint w s) ->
     has_type (Vint b) (Tint w s) ->
@@ -88,20 +88,14 @@ Definition eval_int_bitwise_op (bo : BinOp) (o : Z -> Z -> Z) : Prop :=
     eval_binop_pure (resolve:=resolve) bo (Tint w s) (Tint w s) (Tint w s) (Vint a) (Vint b) (Vint c).
 
 (* arithmetic operators *)
-Axiom eval_add :
-  ltac:(let x := eval hnf in (eval_int_op Badd Z.add) in refine x).
-Axiom eval_sub :
-  ltac:(let x := eval hnf in (eval_int_op Bsub Z.sub) in refine x).
-Axiom eval_mul :
-  ltac:(let x := eval hnf in (eval_int_op Bmul Z.mul) in refine x).
+Axiom eval_add : Hnf (eval_int_op Badd Z.add).
+Axiom eval_sub : Hnf (eval_int_op Bsub Z.sub).
+Axiom eval_mul : Hnf (eval_int_op Bmul Z.mul).
 
 (* bitwise(logical) operators *)
-Axiom eval_or :
-  ltac:(let x := eval hnf in (eval_int_bitwise_op Bor Z.lor) in refine x).
-Axiom eval_and :
-  ltac:(let x := eval hnf in (eval_int_bitwise_op Band Z.land) in refine x).
-Axiom eval_xor :
-  ltac:(let x := eval hnf in (eval_int_bitwise_op Bxor Z.lxor) in refine x).
+Axiom eval_or : Hnf (eval_int_bitwise_op Bor Z.lor).
+Axiom eval_and : Hnf (eval_int_bitwise_op Band Z.land).
+Axiom eval_xor : Hnf (eval_int_bitwise_op Bxor Z.lxor).
 
 (* The binary operator / divides the first operand by the second, after usual
    arithmetic conversions.
@@ -175,7 +169,7 @@ Axiom eval_shr :
    unscoped), usual arithmetic conversions are performed on both
    operands following the rules for arithmetic operators. The values
    are compared after conversions. *)
-Definition eval_int_rel_op (bo : BinOp) {P Q : Z -> Z -> Prop}
+#[local] Definition eval_int_rel_op (bo : BinOp) {P Q : Z -> Z -> Prop}
            (o : forall a b : Z, {P a b} + {Q a b}) : Prop :=
   forall resolve w s a b (av bv : Z) (c : Z),
     a = Vint av ->
@@ -185,7 +179,7 @@ Definition eval_int_rel_op (bo : BinOp) {P Q : Z -> Z -> Prop}
     c = (if o av bv then 1 else 0)%Z ->
     eval_binop_pure (resolve:=resolve) bo (Tint w s) (Tint w s) Tbool a b (Vint c).
 
-Definition eval_int_rel_op_int (bo : BinOp) {P Q : Z -> Z -> Prop}
+#[local] Definition eval_int_rel_op_int (bo : BinOp) {P Q : Z -> Z -> Prop}
            (o : forall a b : Z, {P a b} + {Q a b}) : Prop :=
   forall resolve w s a b (av bv : Z) (c : Z),
     a = Vint av ->
@@ -195,8 +189,7 @@ Definition eval_int_rel_op_int (bo : BinOp) {P Q : Z -> Z -> Prop}
     c = (if o av bv then 1 else 0)%Z ->
     eval_binop_pure (resolve:=resolve) bo (Tint w s) (Tint w s) (T_int) a b (Vint c).
 
-Axiom eval_eq_bool :
-  ltac:(let x := eval hnf in (eval_int_rel_op Beq Z.eq_dec) in refine x).
+Axiom eval_eq_bool : Hnf (eval_int_rel_op Beq Z.eq_dec).
 Axiom eval_neq_bool :
   forall resolve ty a b (av bv : Z) (c : Z),
     a = Vint av ->
@@ -205,31 +198,22 @@ Axiom eval_neq_bool :
     has_type b ty ->
     c = (if Z.eq_dec av bv then 0 else 1)%Z ->
     eval_binop_pure (resolve:=resolve) Bneq ty ty Tbool a b (Vint c).
-Axiom eval_lt_bool :
-  ltac:(let x := eval hnf in (eval_int_rel_op Blt ZArith_dec.Z_lt_ge_dec) in refine x).
-Axiom eval_gt_bool :
-  ltac:(let x := eval hnf in (eval_int_rel_op Bgt ZArith_dec.Z_gt_le_dec) in refine x).
-Axiom eval_le_bool :
-  ltac:(let x := eval hnf in (eval_int_rel_op Ble ZArith_dec.Z_le_gt_dec) in refine x).
-Axiom eval_ge_bool :
-  ltac:(let x := eval hnf in (eval_int_rel_op Bge ZArith_dec.Z_ge_lt_dec) in refine x).
+Axiom eval_lt_bool : Hnf (eval_int_rel_op Blt ZArith_dec.Z_lt_ge_dec).
+Axiom eval_gt_bool : Hnf (eval_int_rel_op Bgt ZArith_dec.Z_gt_le_dec).
+Axiom eval_le_bool : Hnf (eval_int_rel_op Ble ZArith_dec.Z_le_gt_dec).
+Axiom eval_ge_bool : Hnf (eval_int_rel_op Bge ZArith_dec.Z_ge_lt_dec).
 
-Axiom eval_eq_int :
-  ltac:(let x := eval hnf in (eval_int_rel_op_int Beq Z.eq_dec) in refine x).
+Axiom eval_eq_int : Hnf (eval_int_rel_op_int Beq Z.eq_dec).
 Axiom eval_neq_int :
   forall resolve ty a b (av bv : Z) (c : Z),
     a = Vint av ->
     b = Vint bv ->
     c = (if Z.eq_dec av bv then 0 else 1)%Z ->
     eval_binop_pure (resolve:=resolve) Bneq ty ty T_int a b (Vint c).
-Axiom eval_lt_int :
-  ltac:(let x := eval hnf in (eval_int_rel_op_int Blt ZArith_dec.Z_lt_ge_dec) in refine x).
-Axiom eval_gt_int :
-  ltac:(let x := eval hnf in (eval_int_rel_op_int Bgt ZArith_dec.Z_gt_le_dec) in refine x).
-Axiom eval_le_int :
-  ltac:(let x := eval hnf in (eval_int_rel_op_int Ble ZArith_dec.Z_le_gt_dec) in refine x).
-Axiom eval_ge_int :
-  ltac:(let x := eval hnf in (eval_int_rel_op_int Bge ZArith_dec.Z_ge_lt_dec) in refine x).
+Axiom eval_lt_int : Hnf (eval_int_rel_op_int Blt ZArith_dec.Z_lt_ge_dec).
+Axiom eval_gt_int : Hnf (eval_int_rel_op_int Bgt ZArith_dec.Z_gt_le_dec).
+Axiom eval_le_int : Hnf (eval_int_rel_op_int Ble ZArith_dec.Z_le_gt_dec).
+Axiom eval_ge_int : Hnf (eval_int_rel_op_int Bge ZArith_dec.Z_ge_lt_dec).
 
 (* note [Z.lnot a = -1 - a] *)
 Axiom eval_unop_not:
