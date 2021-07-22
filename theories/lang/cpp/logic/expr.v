f040562fdadbc6cc5dcab806aac839cbb28cc6ad
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2021-07-23 01:30:32 +0200

    logic.expr: import prelude.numbers to prepare for moving coercions

--- theories/lang/cpp/logic/expr.v
+++ theories/lang/cpp/logic/expr.v
@@ -7,9 +7,7 @@
  * Semantics of expressions
  * (expressed in weakest pre-condition style)
  *)
-Require Import Coq.Lists.List.
-Require Import Coq.ZArith.BinInt.
-Require Import Coq.NArith.BinNatDef.
+Require Export bedrock.lang.prelude.numbers.
 
 From bedrock.lang.cpp Require Import ast semantics.
 From bedrock.lang.cpp.logic Require Import
