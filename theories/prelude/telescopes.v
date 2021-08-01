1c2f1fcd37eac22298e6d6f5b2a05697ccf77d75
Gregory Malecha <gmalecha@gmail.com>
2021-07-31 23:46:25 -0400

    Lifting combinators for [WithPrePostG] + [add_with_Elaborate].

--- theories/prelude/telescopes.v
+++ theories/prelude/telescopes.v
@@ -17,3 +17,33 @@ Fixpoint tele_append (t : tele) {struct t}: (t -t> tele) -> tele :=
 Definition tele_fun_pointwise@{X Z Y} {t : tele@{X}} {A : Type@{Z}}
     (R : A -> A -> Prop) (f g : tele_fun@{X Z Y} t A) : Prop :=
   forall x, R (tele_app f x) (tele_app g x).
+
+(** [tforallT F] is just like [tforall F] except that [F] returns
+    a [Type] rather than a [Prop].
+
+    To apply this, use [tapplyT].
+ *)
+Fixpoint tforallT {TT : tele} : (TT -> Type) -> Type :=
+  match TT as TT return (TT -> Type) -> Type with
+  | TeleO => fun F => F TargO
+  | TeleS f => fun F => forall x, tforallT (fun arg => F (TargS x arg))
+  end.
+
+Definition targ_0 {T F} (t : tele_arg (@TeleS T F)) : T :=
+  match t in tele_arg X return match X with
+                               | TeleO => unit
+                               | @TeleS x f => x
+                               end
+  with
+  | TargO => tt
+  | TargS x _ => x
+  end.
+
+(** [tapplyT F args] applys [F] to [args] *)
+Fixpoint tapplyT {TT : tele} f (ff : @tforallT TT f) (x: TT) {struct x} : f x :=
+  match x as x in tele_arg X
+        return forall f : X -> Type, tforallT f -> f x
+  with
+  | TargO => fun _ F => F
+  | TargS y t => fun _ F => tapplyT (fun x => _ (TargS y x)) (F y) t
+  end f ff.
