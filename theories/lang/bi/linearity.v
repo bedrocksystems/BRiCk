f767f4a0b467ca32e37ac0d27642b0f3aabaef08
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2021-08-10 19:00:48 +0200

    Also disable `absorbing_bi`

--- theories/lang/bi/linearity.v
+++ theories/lang/bi/linearity.v
@@ -25,6 +25,12 @@ As a small optimization, we can remove dependent instances.
 This causes further incompleteness if other affine logics are relevant.
 *)
 #[export] Remove Hints monPred_bi_affine : typeclass_instances.
+#[export] Remove Hints absorbing_bi : typeclass_instances.
+
+(* Potentially useful to exploit [siProp_affine]. *)
+Module enable_absorbing_bi.
+  #[export] Hint Resolve absorbing_bi : typeclass_instances.
+End enable_absorbing_bi.
 
 Import bi.
 
