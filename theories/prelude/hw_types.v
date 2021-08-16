a5c316a71249556ee535cb2114edf7b46664593b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2021-08-16 02:49:22 +0200

    Refactor hw_types, add cpu.max

--- theories/prelude/hw_types.v
+++ theories/prelude/hw_types.v
@@ -3,35 +3,53 @@
  * This software is distributed under the terms of the BedRock Open-Source License.
  * See the LICENSE-BedRock file in the repository root for details.
  *)
-From Coq Require Import NArith.BinNat.
+From bedrock.prelude Require Import base numbers.
 Require Export bedrock.prelude.wrap.
+
+Module int_line.
+  Include wrapper.
+End int_line.
+
+Module time.
+  Include wrapper.
+End time.
+
+Module cpu.
+  Include wrapper.
+
+  (* Valid CPU IDs range from [0] to [max - 1]. *)
+  Definition max : t := of_N $ pow2N 16.
+End cpu.
+
+Module mmio_idx.
+  Include wrapper.
+End mmio_idx.
+
+Module size_bytes.
+  Include wrapper.
+End size_bytes.
 
 (*
 Module [types] is only used for compatibility with some clients.
-TODO FM-643: flatten this module once clients are migrated.. *)
+TODO FM-643: flatten this module once clients are migrated. *)
 Module Export types.
-  Variant Phant_Int_line : Set := Build_Phant_Int_line.
-  Definition int_line : Set := WrapN Phant_Int_line.
-  Definition Build_int_line (n : N) : int_line := Build_WrapN Phant_Int_line n.
-  Definition get_int_line (s : int_line) : N := s.(unwrapN).
+  Notation int_line := int_line.t (only parsing).
+  Notation Build_int_line := int_line.of_N (only parsing).
+  Notation get_int_line := int_line.to_N (only parsing).
 
-  Variant Phant_Time := Build_Phant_Time.
-  Definition time := WrapN Phant_Time.
-  Definition Build_time (n : N) : time := Build_WrapN Phant_Time n.
-  Definition get_time (t : time) : N := t.(unwrapN).
+  Notation time := time.t (only parsing).
+  Notation Build_time := time.of_N (only parsing).
+  Notation get_time := time.to_N (only parsing).
 
-  Variant Phant_Cpu := Build_Phant_Cpu.
-  Definition cpu := WrapN Phant_Cpu.
-  Definition Build_cpu (n : N) : cpu := Build_WrapN Phant_Cpu n.
-  Definition get_cpu (s : cpu) : N := s.(unwrapN).
+  Notation cpu := cpu.t (only parsing).
+  Notation Build_cpu := cpu.of_N (only parsing).
+  Notation get_cpu := cpu.to_N (only parsing).
 
-  Variant Phant_mmio_idx : Set := Build_Phant_mmio_idx.
-  Definition mmio_idx : Set := WrapN Phant_mmio_idx.
-  Definition Build_mmio_idx (n : N) : mmio_idx := Build_WrapN Phant_mmio_idx n.
-  Definition get_mmio_idx (i : mmio_idx) : N := i.(unwrapN).
+  Notation mmio_idx := mmio_idx.t (only parsing).
+  Notation Build_mmio_idx := mmio_idx.of_N (only parsing).
+  Notation get_mmio_idx := mmio_idx.to_N (only parsing).
 
-  Variant Phant_size_bytes : Set := Build_Phant_size_bytes.
-  Definition size_bytes : Set := WrapN Phant_size_bytes.
-  Definition Build_size_bytes (n : N) : size_bytes := Build_WrapN Phant_size_bytes n.
-  Definition get_size_bytes (sz : size_bytes) : N := sz.(unwrapN).
+  Notation size_bytes := size_bytes.t (only parsing).
+  Notation Build_size_bytes := size_bytes.of_N (only parsing).
+  Notation get_size_bytes := size_bytes.to_N (only parsing).
 End types.
