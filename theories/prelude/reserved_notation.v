2aa8557264fd62c401e512668f791066d1813401
Yoichi Hirai <yoichi@bedrocksystems.com>
2021-11-25 17:48:22 +0100

    Fix copyright header

--- theories/prelude/reserved_notation.v
+++ theories/prelude/reserved_notation.v
@@ -1,8 +1,8 @@
-(**
- * Copyright (C) 2021 BedRock Systems, Inc.
- * All rights reserved.
+(*
+ * Copyright (c) 2020 BedRock Systems, Inc.
  *
- * SPDX-License-Identifier: LGPL-2.1 WITH BedRock Exception for use over network, see repository root for details.
+ * This software is distributed under the terms of the BedRock Open-Source License.
+ * See the LICENSE-BedRock file in the repository root for details.
  *
  *
  * Some of the following code is derived from code original to the
