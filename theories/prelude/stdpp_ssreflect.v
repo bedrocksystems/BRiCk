6299ba13e5fe3825b4e425bf14ebf6b0ee9ff6ad
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2022-01-20 19:12:25 +0100

    Iris bump: Manually apply TODOs

--- theories/prelude/stdpp_ssreflect.v
+++ theories/prelude/stdpp_ssreflect.v
@@ -19,11 +19,7 @@ From stdpp Require Export prelude.
 #[global] Set SsrOldRewriteGoalsOrder. (* See Coq issue #5706 *)
 Ltac done := stdpp.tactics.done.
 
-(* TODO: to enable after the Iris bump. *)
-(*
 (** Iris itself and many dependencies still rely on this coercion. *)
 Coercion Z.of_nat : nat >-> Z.
 
-(* No Hint Mode set in stdpp because of Coq bugs #5735 and #9058, only
-fixed in Coq >= 8.12, which Iris depends on. *)
-#[global] Hint Mode Equiv ! : typeclass_instances. *)
+#[global] Hint Mode Equiv ! : typeclass_instances.
