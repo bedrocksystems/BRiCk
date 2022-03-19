6a5ae4f5fd9f76d0f63a3b0e2f5a6285c1695e4a
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2022-03-19 17:15:12 +0100

    Add prelude.fin_map_dom with new lemma

---
+++ theories/prelude/fin_map_dom.v
@@ -0,0 +1,18 @@
+(*
+ * Copyright (c) 2022 BedRock Systems, Inc.
+ * This software is distributed under the terms of the BedRock Open-Source License.
+ * See the LICENSE-BedRock file in the repository root for details.
+ *)
+
+From stdpp Require Import fin_map_dom.
+From bedrock.prelude Require Import base fin_maps.
+Section fin_map_dom.
+  Context `{FinMapDom K M D}.
+  Context {A : Type}.
+  Implicit Type (m : M A).
+  #[local] Set Default Proof Using "Type*".
+
+  Lemma elem_of_map_to_list_dom m k v :
+    (k, v) ∈ map_to_list m → k ∈ dom D m.
+  Proof. move=> /elem_of_map_to_list. apply elem_of_dom_2. Qed.
+End fin_map_dom.
