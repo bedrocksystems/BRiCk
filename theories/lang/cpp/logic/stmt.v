7afd752f4521decd3ab271155f74977d109802b6
Gregory Malecha <gregory@bedrocksystems.com>
2022-03-21 10:30:07 -0400

    Additional cleanup to the statement semantics.

--- theories/lang/cpp/logic/stmt.v
+++ theories/lang/cpp/logic/stmt.v
@@ -34,15 +34,6 @@ Module Type Stmt.
       destruct rt; try apply H; apply H0.
     Qed.
 
-    (* loop with invariant `I` *)
-    Definition Kloop (I : mpred) (Q : KpredI) : KpredI :=
-      KP (funI rt =>
-          match rt with
-          | Break | Normal => Q Normal
-          | Continue => I
-          | rt => Q rt
-          end).
-
     Definition Kfree (free : FreeTemp) : KpredI -> KpredI :=
       Kat_exit (interp free).
 
@@ -53,41 +44,13 @@ Module Type Stmt.
       iIntros (??) "H"; by iApply interp_frame.
     Qed.
 
-   (* the semantics of return is like an initialization
-     * expression.
-     *)
-    Axiom wp_return_void : forall ρ Q,
-        get_return_type ρ = Tvoid ->
-        Q ReturnVoid |-- wp ρ (Sreturn None) Q.
-
-    Axiom wp_return : forall ρ e (Q : KpredI),
-          (let rty := erase_qualifiers (get_return_type ρ) in
-           wp_call_initialize ρ rty e (fun v _ frees =>
-                                         interp frees (Q (ReturnVal v))))
-           (* ^ NOTE discard [free] because we are extruding the scope of the value *)
-       |-- wp ρ (Sreturn (Some e)) Q.
-
-    Axiom wp_return_frame : forall ρ rv (Q Q' : KpredI),
-        match rv with
-        | None => Q ReturnVoid -* Q' ReturnVoid
-        | Some _ =>
-          (* NOTE unsound in the presence of exceptions *)
-          Forall v, Q (ReturnVal v) -* Q' (ReturnVal v)
-        end |-- wp ρ (Sreturn rv) Q -* wp ρ (Sreturn rv) Q'.
-
-    Axiom wp_break : forall ρ Q,
-        |> Q Break |-- wp ρ Sbreak Q.
-    Axiom wp_break_frame : forall ρ (Q Q' : KpredI),
-        Q Break -* Q' Break |-- wp ρ Sbreak Q -* wp ρ Sbreak Q'.
-
-    Axiom wp_continue : forall ρ Q,
-        |> Q Continue |-- wp ρ Scontinue Q.
-    Axiom wp_continue_frame : forall ρ (Q Q' : KpredI),
-        Q Continue -* Q' Continue |-- wp ρ Scontinue Q -* wp ρ Scontinue Q'.
+    (** * Expression Evaluation *)
 
     Axiom wp_expr : forall ρ vc e Q,
         |> wp_discard ρ vc e (fun free => interp free (Q Normal))
         |-- wp ρ (Sexpr vc e) Q.
+
+    (** * Declarations *)
 
     (* This definition performs allocation of local variables.
      *
@@ -139,8 +102,7 @@ Module Type Stmt.
             end
         in
         if ts then
-          (* thread safe initialization is not currently supported *)
-          False%I
+          UNSUPPORTED "thread safe initialization"
         else
           _global nm |-> tblockR ty 1 ** do_init
       end.
@@ -181,6 +143,8 @@ Module Type Stmt.
         iIntros (??). iApply IHds. iIntros (??) "X". by iApply "a".
     Qed.
 
+    (** * Blocks *)
+
     Fixpoint wp_block (ρ : region) (ss : list Stmt) (Q : KpredI) : mpred :=
       match ss with
       | nil => |> Q Normal
@@ -189,7 +153,6 @@ Module Type Stmt.
       | s :: ss =>
         |> wp ρ s (Kseq (wp_block ρ ss) Q)
       end.
-
 
     Lemma wp_block_frame : forall body ρ (Q Q' : KpredI),
         (Forall rt, Q rt -* Q' rt) |-- wp_block ρ body Q -* wp_block ρ body Q'.
@@ -220,10 +183,12 @@ Module Type Stmt.
     Axiom wp_seq : forall ρ Q ss,
         wp_block ρ ss Q |-- wp ρ (Sseq ss) Q.
 
+    (** [if] *)
+
     Axiom wp_if : forall ρ e thn els Q,
         |> wp_operand ρ e (fun v free =>
              match is_true v with
-             | None => False
+             | None => ERROR "is_true"
              | Some c =>
                interp free $
                if c then
@@ -237,40 +202,58 @@ Module Type Stmt.
         wp ρ (Sseq (Sdecl (d :: nil) :: Sif None e thn els :: nil)) Q
         |-- wp ρ (Sif (Some d) e thn els) Q.
 
-    (* note(gmm): this rule is not sound for a total hoare logic
+    (** * Loops *)
+    (* The loop rules are phrased using loop invariants. An alternative
+     * is to use their 1-step unfoldings and a greatest-fixpoint.
+     *
+     * Inconsistency: Certain infinite loops can be optimized away
+     * in C/C++. E.g. [while (1);] can be optimized to [;]. The loop
+     * rules do not support these.
      *)
+
+    (* loop with invariant `I` *)
+    Definition Kloop (I : mpred) (Q : KpredI) : KpredI :=
+      KP (funI rt =>
+          match rt with
+          | Break => Q Normal
+          | Normal | Continue => I
+          | rt => Q rt
+          end).
+
     Axiom wp_while : forall ρ test body Q I,
-        I |-- wp ρ (Sif None test (Sseq (body :: Scontinue :: nil)) Sskip)
-                (Kloop I Q) ->
+        I |-- wp ρ (Sif None test body Sbreak) (Kloop I Q) ->
         I |-- wp ρ (Swhile None test body) Q.
 
+    (**
+       `while (T x = e) body` desugars to `{ T x = e; while (x) body }`
+     *)
     Axiom wp_while_decl : forall ρ d test body Q,
-        wp ρ (Sseq (Sdecl (d :: nil) :: Swhile None test body :: nil)) Q
+            wp ρ (Sseq (Sdecl (d :: nil) :: Swhile None test body :: nil)) Q
         |-- wp ρ (Swhile (Some d) test body) Q.
 
-
-    (* note(gmm): this rule is not sound for a total hoare logic
-     *)
-    Axiom wp_for : forall ρ test incr b Q Inv,
+    Axiom wp_for : forall ρ test incr body Q I,
+        let incr_I :=
+          match incr with
+          | None => I
+          | Some (vc,incr) => wp_discard ρ vc incr (fun free => interp free I)
+          end
+        in
         match test with
         | None =>
-          Inv |-- wp ρ (Sseq (b :: Scontinue :: nil))
-              (Kloop match incr with
-                     | None => Inv
-                     | Some (vc,incr) => wp_discard ρ vc incr (fun free => interp free Inv)
-                     end Q)
+          I |-- wp ρ body (Kloop incr_I Q)
         | Some test =>
-          Inv |-- wp ρ (Sif None test (Sseq (b :: Scontinue :: nil)) Sskip)
-              (Kloop match incr with
-                     | None => Inv
-                     | Some (vc,incr) => wp_discard ρ vc incr (fun free => interp free Inv)
-                     end Q)
+          I |-- wp ρ (Sif None test body Sbreak) (Kloop incr_I Q)
         end ->
-        Inv |-- wp ρ (Sfor None test incr b) Q.
+        I |-- wp ρ (Sfor None test incr body) Q.
 
+    (**
+       `for (init; test; incr) body` desugars to `{ init; for (; test; incr) body }`
+     *)
     Axiom wp_for_init : forall ρ init test incr b Q,
-        wp ρ (Sseq (init :: Sfor None test incr b :: nil)) Q
+            wp ρ (Sseq (init :: Sfor None test incr b :: nil)) Q
         |-- wp ρ (Sfor (Some init) test incr b) Q.
+
+    (** ** `do` loops *)
 
     Definition Kdo (ρ : region) (e : Expr) (I : mpred) (Q : KpredI) : KpredI :=
       KP (funI rt =>
@@ -279,15 +262,53 @@ Module Type Stmt.
           | Continue | Normal =>
             wp_operand ρ e (fun v free =>
                               match is_true v with
-                              | None => False
+                              | None => ERROR "is_true"
                               | Some c => interp free $ if c then I else Q Normal
                               end)
           | rt => Q rt
           end).
 
     Axiom wp_do : forall ρ test body Q I,
-        I |-- wp ρ (Sseq (body :: nil)) (Kdo ρ test I Q) ->
+        I |-- wp ρ body (Kdo ρ test I Q) ->
         I |-- wp ρ (Sdo body test) Q.
+
+    (** * Return *)
+
+    (* the semantics of return is like an initialization
+     * expression.
+     *)
+    Axiom wp_return_void : forall ρ Q,
+        get_return_type ρ = Tvoid ->
+        Q ReturnVoid |-- wp ρ (Sreturn None) Q.
+
+    Axiom wp_return : forall ρ e (Q : KpredI),
+          (let rty := erase_qualifiers (get_return_type ρ) in
+           wp_call_initialize ρ rty e (fun v _ frees =>
+                                         interp frees (Q (ReturnVal v))))
+           (* ^ NOTE discard [free] because we are extruding the scope of the value *)
+       |-- wp ρ (Sreturn (Some e)) Q.
+
+    Axiom wp_return_frame : forall ρ rv (Q Q' : KpredI),
+        match rv with
+        | None => Q ReturnVoid -* Q' ReturnVoid
+        | Some _ =>
+          (* NOTE unsound in the presence of exceptions *)
+          Forall v, Q (ReturnVal v) -* Q' (ReturnVal v)
+        end |-- wp ρ (Sreturn rv) Q -* wp ρ (Sreturn rv) Q'.
+
+    (** * Control flow: `break`, `continue` *)
+
+    Axiom wp_break : forall ρ Q,
+        |> Q Break |-- wp ρ Sbreak Q.
+    Axiom wp_break_frame : forall ρ (Q Q' : KpredI),
+        Q Break -* Q' Break |-- wp ρ Sbreak Q -* wp ρ Sbreak Q'.
+
+    Axiom wp_continue : forall ρ Q,
+        |> Q Continue |-- wp ρ Scontinue Q.
+    Axiom wp_continue_frame : forall ρ (Q Q' : KpredI),
+        Q Continue -* Q' Continue |-- wp ρ Scontinue Q -* wp ρ Scontinue Q'.
+
+    (** `switch` *)
 
     (* compute the [Prop] that is known if this switch branch is taken *)
     Definition wp_switch_branch (s : SwitchBranch) (v : Z) : Prop :=
