7602e412390a1eecf5aa84920d27616ef30eb957
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2022-03-30 15:32:33 +0200

    primitives: drop not-quite-implied %Z

--- theories/lang/cpp/primitives.v
+++ theories/lang/cpp/primitives.v
@@ -12,38 +12,38 @@ Notation "'boolR' q v" := (@primR _ _ _ Tbool q (Vbool v)) (at level 10, q at le
 (* note(gmm): these might need to become definitions if we want to be generic
  * across sizes
  *)
-Notation "'scharR' q v" := (@primR _ _ _ (Tschar) q (Vint v%Z)) (at level 10, q at level 1, v at level 1).
-Notation "'ucharR' q v" := (@primR _ _ _ (Tuchar) q (Vint v%Z)) (at level 10, q at level 1, v at level 1).
+Notation "'scharR' q v" := (@primR _ _ _ (Tschar) q (Vint v)) (at level 10, q at level 1, v at level 1).
+Notation "'ucharR' q v" := (@primR _ _ _ (Tuchar) q (Vint v)) (at level 10, q at level 1, v at level 1).
 Notation "'charR' q v" := (ucharR q v) (at level 10, q at level 1, v at level 1).
 
-Notation "'sshortR' q v" := (@primR _ _ _ (Tint W16 Signed) q (Vint v%Z)) (only parsing, at level 10, q at level 1, v at level 1).
-Notation "'ushortR' q v" := (@primR _ _ _ (Tint W16 Unsigned) q (Vint v%Z)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'sshortR' q v" := (@primR _ _ _ (Tint W16 Signed) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'ushortR' q v" := (@primR _ _ _ (Tint W16 Unsigned) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
 Notation "'shortR' q v" := (sshortR q v) (only parsing, at level 10, q at level 1, v at level 1).
 
 
-Notation "'sintR' q v" := (@primR _ _ _ (Tint int_bits Signed) q (Vint v%Z)) (only parsing, at level 10, q at level 1, v at level 1).
-Notation "'uintR' q v" := (@primR _ _ _ (Tint int_bits Unsigned) q (Vint v%Z)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'sintR' q v" := (@primR _ _ _ (Tint int_bits Signed) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'uintR' q v" := (@primR _ _ _ (Tint int_bits Unsigned) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
 Notation "'intR' q v" := (sintR q v) (only parsing, at level 10, q at level 1, v at level 1).
 
-Notation "'slongR' q v" := (@primR _ _ _ (Tint long_bits Signed) q (Vint v%Z)) (only parsing, at level 10, q at level 1, v at level 1).
-Notation "'ulongR' q v" := (@primR _ _ _ (Tint long_bits Unsigned) q (Vint v%Z)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'slongR' q v" := (@primR _ _ _ (Tint long_bits Signed) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'ulongR' q v" := (@primR _ _ _ (Tint long_bits Unsigned) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
 Notation "'longR' q v" := (slongR q v) (only parsing, at level 10, q at level 1, v at level 1).
 
-Notation "'slonglongR' q v" := (@primR _ _ _ (Tint W64 Signed) q (Vint v%Z)) (only parsing, at level 10, q at level 1, v at level 1).
-Notation "'ulonglongR' q v" := (@primR _ _ _ (Tint W64 Unsigned) q (Vint v%Z)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'slonglongR' q v" := (@primR _ _ _ (Tint W64 Signed) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'ulonglongR' q v" := (@primR _ _ _ (Tint W64 Unsigned) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
 Notation "'longlongR' q v" := (slonglongR q v) (only parsing, at level 10, q at level 1, v at level 1).
 
 (* integers with explict sizes *)
 
-Notation "'u8R'  q v" := (primR Tu8  q (Vint v%Z)) (at level 10, q at level 1, v at level 1).
-Notation "'u16R' q v" := (primR Tu16 q (Vint v%Z)) (at level 10, q at level 1, v at level 1).
-Notation "'u32R' q v" := (primR Tu32 q (Vint v%Z)) (at level 10, q at level 1, v at level 1).
-Notation "'u64R' q v" := (primR Tu64 q (Vint v%Z)) (at level 10, q at level 1, v at level 1).
+Notation "'u8R'  q v" := (primR Tu8  q (Vint v)) (at level 10, q at level 1, v at level 1).
+Notation "'u16R' q v" := (primR Tu16 q (Vint v)) (at level 10, q at level 1, v at level 1).
+Notation "'u32R' q v" := (primR Tu32 q (Vint v)) (at level 10, q at level 1, v at level 1).
+Notation "'u64R' q v" := (primR Tu64 q (Vint v)) (at level 10, q at level 1, v at level 1).
 
-Notation "'i8R'  q v" := (primR Ti8  q (Vint v%Z)) (at level 10, q at level 1, v at level 1).
-Notation "'i16R' q v" := (primR Ti16 q (Vint v%Z)) (at level 10, q at level 1, v at level 1).
-Notation "'i32R' q v" := (primR Ti32 q (Vint v%Z)) (at level 10, q at level 1, v at level 1).
-Notation "'i64R' q v" := (primR Ti64 q (Vint v%Z)) (at level 10, q at level 1, v at level 1).
+Notation "'i8R'  q v" := (primR Ti8  q (Vint v)) (at level 10, q at level 1, v at level 1).
+Notation "'i16R' q v" := (primR Ti16 q (Vint v)) (at level 10, q at level 1, v at level 1).
+Notation "'i32R' q v" := (primR Ti32 q (Vint v)) (at level 10, q at level 1, v at level 1).
+Notation "'i64R' q v" := (primR Ti64 q (Vint v)) (at level 10, q at level 1, v at level 1).
 
 #[deprecated(since="2022-04-1", note="use [u8R]")]
 Notation "'uint8R' q v" := (ucharR q v) (only parsing, at level 10, q at level 1, v at level 1).
