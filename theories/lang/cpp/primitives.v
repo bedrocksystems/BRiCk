b2a9af818c91e881593cfa07f32c4a81742c6476
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2022-03-30 15:33:02 +0200

    Replace `@primR _ _ _` with `primR`

--- theories/lang/cpp/primitives.v
+++ theories/lang/cpp/primitives.v
@@ -6,31 +6,31 @@
 Require Import bedrock.lang.cpp.ast.
 Require Import bedrock.lang.cpp.logic.
 
-Notation "'boolR' q v" := (@primR _ _ _ Tbool q (Vbool v)) (at level 10, q at level 1, v at level 1).
+Notation "'boolR' q v" := (primR Tbool q (Vbool v)) (at level 10, q at level 1, v at level 1).
 
 (* integers with implicit sizes *)
 (* note(gmm): these might need to become definitions if we want to be generic
  * across sizes
  *)
-Notation "'scharR' q v" := (@primR _ _ _ (Tschar) q (Vint v)) (at level 10, q at level 1, v at level 1).
-Notation "'ucharR' q v" := (@primR _ _ _ (Tuchar) q (Vint v)) (at level 10, q at level 1, v at level 1).
+Notation "'scharR' q v" := (primR (Tschar) q (Vint v)) (at level 10, q at level 1, v at level 1).
+Notation "'ucharR' q v" := (primR (Tuchar) q (Vint v)) (at level 10, q at level 1, v at level 1).
 Notation "'charR' q v" := (ucharR q v) (at level 10, q at level 1, v at level 1).
 
-Notation "'sshortR' q v" := (@primR _ _ _ (Tint W16 Signed) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
-Notation "'ushortR' q v" := (@primR _ _ _ (Tint W16 Unsigned) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'sshortR' q v" := (primR (Tint W16 Signed) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'ushortR' q v" := (primR (Tint W16 Unsigned) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
 Notation "'shortR' q v" := (sshortR q v) (only parsing, at level 10, q at level 1, v at level 1).
 
 
-Notation "'sintR' q v" := (@primR _ _ _ (Tint int_bits Signed) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
-Notation "'uintR' q v" := (@primR _ _ _ (Tint int_bits Unsigned) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'sintR' q v" := (primR (Tint int_bits Signed) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'uintR' q v" := (primR (Tint int_bits Unsigned) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
 Notation "'intR' q v" := (sintR q v) (only parsing, at level 10, q at level 1, v at level 1).
 
-Notation "'slongR' q v" := (@primR _ _ _ (Tint long_bits Signed) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
-Notation "'ulongR' q v" := (@primR _ _ _ (Tint long_bits Unsigned) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'slongR' q v" := (primR (Tint long_bits Signed) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'ulongR' q v" := (primR (Tint long_bits Unsigned) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
 Notation "'longR' q v" := (slongR q v) (only parsing, at level 10, q at level 1, v at level 1).
 
-Notation "'slonglongR' q v" := (@primR _ _ _ (Tint W64 Signed) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
-Notation "'ulonglongR' q v" := (@primR _ _ _ (Tint W64 Unsigned) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'slonglongR' q v" := (primR (Tint W64 Signed) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
+Notation "'ulonglongR' q v" := (primR (Tint W64 Unsigned) q (Vint v)) (only parsing, at level 10, q at level 1, v at level 1).
 Notation "'longlongR' q v" := (slonglongR q v) (only parsing, at level 10, q at level 1, v at level 1).
 
 (* integers with explict sizes *)
@@ -64,6 +64,6 @@ Notation "'int32R' q v" := (intR q v) (only parsing, at level 10, q at level 1, 
 Notation "'int64R' q v" := (longlongR q v) (only parsing, at level 10, q at level 1, v at level 1).
 
 
-Notation "'ptrR<' ty '>' q p" := (@primR _ _ _ (Tpointer ty) q (Vptr p)) (at level 10, ty at level 20, q at level 1, p at level 1, format "'ptrR<' ty '>'  q  p").
+Notation "'ptrR<' ty '>' q p" := (primR (Tpointer ty) q (Vptr p)) (at level 10, ty at level 20, q at level 1, p at level 1, format "'ptrR<' ty '>'  q  p").
 
 Notation "'refR<' ty '>' p" := (primR (Tref ty) (Vptr p)) (at level 10, ty at level 20, p at level 1, format "'refR<' ty '>'  p").
