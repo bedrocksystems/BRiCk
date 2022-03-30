0dd1451e2cb9d58a8b6cb442cc2cd9cbfb726d38
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2022-03-30 15:51:54 +0200

    primitives.v: reorder (without changes)

--- theories/lang/cpp/primitives.v
+++ theories/lang/cpp/primitives.v
@@ -8,31 +8,6 @@ Require Import bedrock.lang.cpp.logic.
 
 Notation boolR q v := (primR Tbool q (Vbool v)).
 
-(* integers with implicit sizes *)
-(* note(gmm): these might need to become definitions if we want to be generic
- * across sizes
- *)
-Notation scharR q v := (primR (Tschar) q (Vint v)) (only parsing).
-Notation ucharR q v := (primR (Tuchar) q (Vint v)) (only parsing).
-Notation charR q v := (ucharR q v) (only parsing).
-
-Notation sshortR q v := (primR (Tint W16 Signed) q (Vint v)) (only parsing).
-Notation ushortR q v := (primR (Tint W16 Unsigned) q (Vint v)) (only parsing).
-Notation shortR q v := (sshortR q v) (only parsing).
-
-
-Notation sintR q v := (primR (Tint int_bits Signed) q (Vint v)) (only parsing).
-Notation uintR q v := (primR (Tint int_bits Unsigned) q (Vint v)) (only parsing).
-Notation intR q v := (sintR q v) (only parsing).
-
-Notation slongR q v := (primR (Tint long_bits Signed) q (Vint v)) (only parsing).
-Notation ulongR q v := (primR (Tint long_bits Unsigned) q (Vint v)) (only parsing).
-Notation longR q v := (slongR q v) (only parsing).
-
-Notation slonglongR q v := (primR (Tint W64 Signed) q (Vint v)) (only parsing).
-Notation ulonglongR q v := (primR (Tint W64 Unsigned) q (Vint v)) (only parsing).
-Notation longlongR q v := (slonglongR q v) (only parsing).
-
 (* integers with explict sizes *)
 
 Notation u8R  q v := (primR Tu8  q (Vint v)).
@@ -62,6 +37,31 @@ Notation int16R q v := (i16R q v) (only parsing).
 Notation int32R q v := (i32R q v) (only parsing).
 #[deprecated(since="2022-04-1", note="use [i64R]")]
 Notation int64R q v := (i64R q v) (only parsing).
+
+(* integers with implicit sizes *)
+(* note(gmm): these might need to become definitions if we want to be generic
+ * across sizes
+ *)
+Notation scharR q v := (primR (Tschar) q (Vint v)) (only parsing).
+Notation ucharR q v := (primR (Tuchar) q (Vint v)) (only parsing).
+Notation charR q v := (ucharR q v) (only parsing).
+
+Notation sshortR q v := (primR (Tint W16 Signed) q (Vint v)) (only parsing).
+Notation ushortR q v := (primR (Tint W16 Unsigned) q (Vint v)) (only parsing).
+Notation shortR q v := (sshortR q v) (only parsing).
+
+
+Notation sintR q v := (primR (Tint int_bits Signed) q (Vint v)) (only parsing).
+Notation uintR q v := (primR (Tint int_bits Unsigned) q (Vint v)) (only parsing).
+Notation intR q v := (sintR q v) (only parsing).
+
+Notation slongR q v := (primR (Tint long_bits Signed) q (Vint v)) (only parsing).
+Notation ulongR q v := (primR (Tint long_bits Unsigned) q (Vint v)) (only parsing).
+Notation longR q v := (slongR q v) (only parsing).
+
+Notation slonglongR q v := (primR (Tint W64 Signed) q (Vint v)) (only parsing).
+Notation ulonglongR q v := (primR (Tint W64 Unsigned) q (Vint v)) (only parsing).
+Notation longlongR q v := (slonglongR q v) (only parsing).
 
 
 Notation "'ptrR<' ty '>' q p" := (primR (Tpointer ty) q (Vptr p)) (at level 10, ty at level 20, q at level 1, p at level 1, format "'ptrR<' ty '>'  q  p").
