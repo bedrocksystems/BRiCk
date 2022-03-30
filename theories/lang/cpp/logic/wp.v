d3b301c535b4f042618419e90ad191850afce969
Jan-Oliver Kaiser <janno@bedrocksystems.com>
2022-03-30 10:27:08 +0000

    Add notation for `region`s.

--- theories/lang/cpp/logic/wp.v
+++ theories/lang/cpp/logic/wp.v
@@ -240,6 +240,29 @@ Definition _this (ρ : region) : ptr :=
   end.
 Arguments _this !_ / : assert.
 
+(** Region notations
+ *)
+#[global] Declare Custom Entry cpp_region.
+
+Notation "'return' ty" :=
+  (Remp None ty)
+  (in custom cpp_region at level 0, ty constr,
+   format "'[  ' return  ty ']'").
+Notation "[ 'this' := ptr ] ; 'return' ty" :=
+  (Remp (Some ptr) ty)
+  (in custom cpp_region at level 1, ptr constr, ty constr,
+   format "'[' [ this  :=  ptr ] ']' ;  '/' '[  ' return  '/' ty ']'").
+Notation "v @ p ';' ρ " :=
+  (Rbind v p ρ)
+  (in custom cpp_region at level 1, v constr, p constr, ρ custom cpp_region at level 1,
+   format "'[' v  @  p ']' ;  '/' ρ").
+Notation "'[region:' ρ ]" :=
+  (ρ)
+  (at level 0, ρ custom cpp_region at level 1,
+   format "'[hv' [region:  '/ ' '[' ρ ']' ']' ]").
+
+
+
 Module WPE.
 Section with_cpp.
   Context `{Σ : cpp_logic thread_info}.
