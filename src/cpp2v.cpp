9417f7675609d3baadd406c2392f53a17fbf70d8
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2022-07-09 03:32:10 +0200

    Fix whitespace

--- src/cpp2v.cpp
+++ src/cpp2v.cpp
@@ -1,8 +1,8 @@
 /*
  * Copyright (c) 2020 BedRock Systems, Inc.
  *
- * This software is distributed under the terms of the BedRock Open-Source License. 
- * See the LICENSE-BedRock file in the repository root for details. 
+ * This software is distributed under the terms of the BedRock Open-Source License.
+ * See the LICENSE-BedRock file in the repository root for details.
  *
  * This file is based on the tutorial here:
  * https://clang.llvm.org/docs/LibASTMatchersTutorial.html
