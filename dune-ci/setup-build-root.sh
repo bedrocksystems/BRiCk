07606444ea27f75867e089b72c27f280a1148f7b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2022-08-15 02:59:27 +0200

    Fix FM-2813: ensure matching flags to avoid rebuilding cpp2v-core

--- dune-ci/setup-build-root.sh
+++ dune-ci/setup-build-root.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+# Temporary workaround for FM-2813; will be fixed better with FM-2681
 set -ex
 brick_path=fmdeps/cpp2v-core
 cp ${brick_path}/dune-ci/root-dune-project dune-project
+cp ${brick_path}/dune-ci/root-dune dune
+mkdir -p support/fm/
+cp ${brick_path}/dune-ci/coqc_timing.sh support/fm/coqc_timing.sh
 ln -sf ${brick_path}/coq-cpp2v-bin.opam coq-cpp2v-bin.opam
