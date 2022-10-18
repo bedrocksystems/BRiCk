fafceb8a18d96a2d76b49f425b37072a416e86ba
Rodolphe Lepigre <rodolphe@bedrocksystems.com>
2022-10-18 09:34:04 +0200

    CI: remove unused script.

--- dune-ci/setup-build-root.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# We simulate a dune project composition to work around https://github.com/ocaml/dune/issues/6005.
-set -ex
-brick_path=fmdeps/cpp2v-core
-cp ${brick_path}/dune-ci/root-dune-project dune-project
