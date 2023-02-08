ae57de3058ccb95ad614975fad1b1831af0dead6
Jasper Haag <jasper@bedrocksystems.com>
2023-02-08 13:48:32 -0500

    documentation: remove [alectryon] as a submodule in favor of depending upon the [alectryon] v1.4.0 python package

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -43,7 +43,6 @@ sys.setrecursionlimit(10000)
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.append(os.path.abspath('../../alectryon/'))
 
 # -- Prolog ------------------------------------------------------------------
 
@@ -202,7 +201,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', '../../alectryon/alectryon/assets']
+html_static_path = ['_static']
 
 html_css_files = [
     'css/justify.css',
