9dc5b36afea6cb6f1e26edc6c53165d0be295941
Gregory Malecha <gregory@bedrocksystems.com>
2023-03-29 09:56:50 -0400

    Dispatch does not depend on [wp].

--- theories/lang/cpp/logic/dispatch.v
+++ theories/lang/cpp/logic/dispatch.v
@@ -10,7 +10,7 @@ Require Import iris.proofmode.proofmode.
 Require Import bedrock.lang.cpp.ast.
 Require Import bedrock.lang.cpp.semantics.
 From bedrock.lang.cpp.logic Require Import pred heap_pred path_pred.
-From bedrock.lang.cpp.logic Require Import wp translation_unit.
+From bedrock.lang.cpp.logic Require Import translation_unit.
 
 Section with_cpp.
   Context `{Σ : cpp_logic}.
