2a7ce20fcad76cf1635d57f54ab1a2748defbcd3
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2023-05-19 22:56:03 +0200

    Move code into rep_defs (mostly unchanged)

--- theories/lang/cpp/logic/rep_defs.v
+++ theories/lang/cpp/logic/rep_defs.v
@@ -8,7 +8,7 @@
 This file defines the core type [Rep] of representation predicates, for use in [heap_notations].
 *)
 From bedrock.lang.bi Require Import prelude monpred.
-From bedrock.lang.cpp Require Import semantics.values logic.mpred.
+From bedrock.lang.cpp Require Import semantics.values logic.mpred heap_notations.
 
 Import ChargeNotation.
 Implicit Types (σ resolve : genv) (p : ptr) (o : offset).
@@ -66,3 +66,101 @@ End defs.
 #[global] Instance: Params (@as_Rep) 2 := {}.
 #[global] Instance: Params (@_offsetR) 3 := {}.
 #[global] Instance: Params (@pureR) 2 := {}.
+
+Canonical Structure AT_ptr `{Σ : cpp_logic} : AT :=
+  {| AT_LHS := ptr; AT_RHS := Rep; AT_Result := mpred; AT_at := _at |}.
+Canonical Structure AT_offset `{Σ : cpp_logic} : AT :=
+  {| AT_LHS := offset; AT_RHS := Rep; AT_Result := Rep; AT_at := _offsetR |}.
+
+#[global] Notation _at := (@__at AT_ptr) (only parsing).
+#[global] Notation _offsetR := (@__at AT_offset) (only parsing).
+
+Module INTERNAL.
+  Ltac unfold_at := rewrite __at.unlock /AT_at/=; try rewrite rep_defs._at_eq; try rewrite rep_defs._offsetR_eq.
+  Lemma _at_eq `{Σ : cpp_logic} p R : p |-> R = rep_defs._at_def p R.
+  Proof. by unfold_at. Qed.
+  Lemma _offsetR_eq `{Σ : cpp_logic} o R : o |-> R = rep_defs._offsetR_def o R.
+  Proof. by unfold_at. Qed.
+End INTERNAL.
+
+(* Test suite *)
+Module Type TEST_SUITE.
+Section test_suite.
+  Context {σ : genv} `{Σ : cpp_logic} (R : Rep) (f g : field) (o : offset) (p : ptr) (v : val).
+
+  #[local] Ltac syntactically_equal :=
+    lazymatch goal with
+    | _ : _ |- ?X = ?X => idtac
+    end;
+    reflexivity.
+
+  Goal o ., f = o ,, _field f.
+  Proof. syntactically_equal. Qed.
+
+  Goal p |-> R = _at p R.
+  Proof. syntactically_equal. Qed.
+
+  Goal o |-> R = _offsetR o R.
+  Proof. syntactically_equal. Qed.
+
+  (*Example _0 := |> p |-> R.
+
+  Example _1 := |> p ., f |-> R.
+
+  Example _1' := |> _eqv v ., f |-> R.*)
+
+  Example _2 := p |-> _field f |-> R.
+
+  Example _3 := p .[ Tint ! 0 ] |-> R.
+
+  Example _4 := p |-> _field f .[ Tint ! 0 ] |-> R.
+
+  Example _4a := p ., f |-> R.
+
+  Example _4b : p ., f .[ Tint ! 0] = (p ., f) .[ Tint ! 0].
+  Proof. syntactically_equal. Qed.
+
+  Example _5 := p .[ Tint ! 0 ] .[ Tint ! 3 ] |-> R.
+
+  Example _6 := p ., f .[ Tint ! 0 ] ., g |-> R.
+
+  Example _7 := p ., g ., f .[ Tint ! 1 ] .[ Tint ! 0 ] ., f |-> _field f |-> R.
+
+  Example _8 := p ., g ., f .[ Tint ! 1 ] .[ Tint ! 0 ] ., f |-> .[ Tint ! 1 ] |-> R.
+
+  Example _9 := o ., g ., f .[ Tint ! 1 ] .[ Tint ! 0 ] ., f |-> R.
+
+  Example _10 := o ., g ., f .[ Tint ! 1 ] .[ Tint ! 0 ] ., f |-> R.
+
+  Example _11 := o .[ Tint ! 1 ] |-> R.
+
+  Example _12 := o .[ Tint ! 1 ] |-> R.
+
+  (*Example _13 := _eqv v .[ Tint ! 1 ] |-> R.
+
+  Example _13' := _eqv v |-> R.*)
+
+  Example _14 := .[ Tint ! 1 ] |-> R.
+
+  (*Example _15 := |> .[ Tint ! 1 ] |-> |> R.*)
+
+  Example _16 := p .[ Tint ! 1] |-> _offsetR (_field f) R.
+
+  (*backwards compatibility*)
+  Example b0 := _at p R.
+
+  Example b1 := _offsetR (_field f) R.
+
+  Example b2 := _at (p .[ Tint ! 1]) R.
+
+  Example b3 := _at (p .[ Tint ! 1]) (_offsetR (_field f) R).
+
+  (*failure cases*)
+  (*Fail Example fail0 := p |-> ▷ R ∗ R.
+  (*Fail Example fail1 := p |-> |> R ** R. (* requires parsing as [(p |-> |> R) ** R] *)*)
+
+  Fail Example fail2 := p |-> R ** R.*) (* requires parsing as [(p |-> R) ** R] *)
+
+  Fail Example fail3 := p |-> R q.
+End test_suite.
+End TEST_SUITE.
