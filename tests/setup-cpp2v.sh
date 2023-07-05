7be2ed81e7333ca98503154e203f6513025568f8
Gregory Malecha <gregory@bedrocksystems.com>
2023-07-05 07:10:54 -0400

    Extend the testing infrastructure to allow testing different c++ versions.

--- tests/setup-cpp2v.sh
+++ tests/setup-cpp2v.sh
@@ -1,15 +1,34 @@
 export COQPATH="$DUNE_SOURCEROOT/_build/install/default/lib/coq/user-contrib"
 
+check_cpp2v_versions() {
+    input="$1"
+    base="${input%.*}"
+
+    shift
+    for ver in "$@"
+    do
+        echo "cpp2v -v -names ${base}_${ver}_cpp_names.v -o ${base}_${ver}_cpp.v ${input} -- -std=c++${ver}"
+        cpp2v -v -names ${base}_${ver}_cpp_names.v -o ${base}_${ver}_cpp.v ${input} -- -std=c++${ver}
+
+        echo "coqc -w -notation-overridden ${base}_${ver}_cpp_names.v"
+        coqc -w -notation-overridden "${base}_${ver}_cpp_names.v"
+
+        echo "coqc -w -notation-overridden ${base}_${ver}_cpp.v"
+        coqc -w -notation-overridden "${base}_${ver}_cpp.v"
+    done
+}
+
 check_cpp2v() {
-  input="$1"
-  base="${input%.*}"
+    input="$1"
+    base="${input%.*}"
+    ver="17"
 
-  echo "cpp2v -v -names ${base}_cpp_names.v -o ${base}_cpp.v ${input} -- -std=c++17"
-  cpp2v -v -names ${base}_cpp_names.v -o ${base}_cpp.v ${input} -- -std=c++17
+    echo "cpp2v -v -names ${base}_cpp_names.v -o ${base}_cpp.v ${input} -- -std=c++${ver}"
+    cpp2v -v -names ${base}_cpp_names.v -o ${base}_cpp.v ${input} -- -std=c++${ver}
 
-  echo "coqc -w -notation-overridden ${base}_cpp_names.v"
-  coqc -w -notation-overridden "${base}_cpp_names.v"
+    echo "coqc -w -notation-overridden ${base}_cpp_names.v"
+    coqc -w -notation-overridden "${base}_cpp_names.v"
 
-  echo "coqc -w -notation-overridden ${base}_cpp.v"
-  coqc -w -notation-overridden "${base}_cpp.v"
+    echo "coqc -w -notation-overridden ${base}_cpp.v"
+    coqc -w -notation-overridden "${base}_cpp.v"
 }
