6c466dc8774b6f23bc5d7937ef4faa8a2cf3de03
David Swasey <david@bedrocksystems.com>
2023-08-10 12:42:43 -0400

    syntax: Hoist type operations
    Aim: Make typing.v about C++ typing, not C++ types.
    
    Also,
    
    - Make `drop_reference` more robust wrt qualified, non-reference types

--- theories/lang/cpp/syntax/types.v
+++ theories/lang/cpp/syntax/types.v
@@ -543,6 +543,7 @@ Proof. solve_proper. Qed.
 Lemma Qmut_equiv t : Qmut t ≡ t.
 Proof. by rewrite /Qmut Tqualified_id. Qed.
 
+(** ** Qualifier normalization *)
 (**
 [decompose_type t] strips any top-level qualifiers from [t] and
 returns them, paired with the rest of the type.
@@ -571,6 +572,16 @@ Definition decompose_type : type -> type_qualifiers * type :=
   qual_norm (fun q t => (q, t)).
 #[global] Hint Opaque decompose_type : typeclass_instances.
 #[global] Arguments decompose_type !_ / : simpl nomatch, assert.
+
+(**
+[drop_qualifiers t] drops all the *leading* qualifiers of the type [t],
+e.g. [drop_qualifiers (Qconst (Qmut t)) = t].
+ *)
+Fixpoint drop_qualifiers (t : type) : type :=
+  match t with
+  | Tqualified _ t => drop_qualifiers t
+  | _ => t
+  end.
 
 (**
 It would be nice to make this the default.
@@ -756,7 +767,167 @@ Proof.
   by rewrite left_id_L.
 Qed.
 
-(** Smart constructors *)
+(** [drop_qualifeirs] *)
+
+Lemma is_qualified_drop_qualifiers ty : ~~ is_qualified (drop_qualifiers ty).
+Proof. by induction ty. Qed.
+#[global] Hint Resolve is_qualified_drop_qualifiers | 0 : core.
+
+Lemma drop_qualifiers_unqual t : ~~ is_qualified t -> drop_qualifiers t = t.
+Proof. by destruct t; cbn; auto. Qed.
+
+Lemma drop_qualifiers_qual_norm' q t : drop_qualifiers t = qual_norm' (fun _ t => t) q t.
+Proof.
+  elim: (qual_norm'_ok _ q t).
+  { intros. by rewrite drop_qualifiers_unqual. }
+  { done. }
+Qed.
+Lemma drop_qualifiers_qual_norm t : drop_qualifiers t = qual_norm (fun _ t => t) t.
+Proof. apply drop_qualifiers_qual_norm'. Qed.
+Lemma drop_qualifiers_decompose_type t : drop_qualifiers t = (decompose_type t).2.
+Proof. by rewrite drop_qualifiers_qual_norm qual_norm_decompose_type. Qed.
+
+Lemma drop_qualifiers_idemp t : drop_qualifiers (drop_qualifiers t) = drop_qualifiers t.
+Proof. by rewrite drop_qualifiers_unqual. Qed.
+
+Lemma unqual_drop_qualifiers ty tq ty' : drop_qualifiers ty <> Tqualified tq ty'.
+Proof. by induction ty. Qed.
+
+(** ** Erasing qualifiers *)
+(**
+[erase_qualifiers t] erases *all* qualifiers that occur everywhere in
+the type.
+
+NOTE we currently use this because we do not track [const]ness in the
+logic, this is somewhat reasonable because we often opt to express
+this in separation logic. And the type system also enforces some of
+the other criteria.
+*)
+Fixpoint erase_qualifiers (t : type) : type :=
+  match t with
+  | Tpointer t => Tpointer (erase_qualifiers t)
+  | Tref t => Tref (erase_qualifiers t)
+  | Trv_ref t => Trv_ref (erase_qualifiers t)
+  | Tnum _ _
+  | Tchar_ _
+  | Tbool
+  | Tvoid
+  | Tfloat_ _
+  | Tnamed _
+  | Tenum _ => t
+  | Tarray t sz => Tarray (erase_qualifiers t) sz
+  | @Tfunction cc ar t ts => Tfunction (cc:=cc) (ar:=ar) (erase_qualifiers t) (List.map erase_qualifiers ts)
+  | Tmember_pointer cls t => Tmember_pointer cls (erase_qualifiers t)
+  | Tqualified _ t => erase_qualifiers t
+  | Tnullptr => Tnullptr
+  | Tarch sz nm => Tarch sz nm
+  end.
+
+Lemma is_qualified_erase_qualifiers ty : ~~ is_qualified (erase_qualifiers ty).
+Proof. by induction ty. Qed.
+#[global] Hint Resolve is_qualified_erase_qualifiers | 0 : core.
+
+Lemma erase_qualifiers_qual_norm' q t :
+  erase_qualifiers t = qual_norm' (fun _ t => erase_qualifiers t) q t.
+Proof. by elim: (qual_norm'_ok _ q t). Qed.
+Lemma erase_qualifiers_qual_norm t :
+  erase_qualifiers t = qual_norm (fun _ t => erase_qualifiers t) t.
+Proof. apply erase_qualifiers_qual_norm'. Qed.
+Lemma erase_qualifiers_decompose_type t :
+  erase_qualifiers t = erase_qualifiers (decompose_type t).2.
+Proof. by rewrite erase_qualifiers_qual_norm qual_norm_decompose_type. Qed.
+
+Lemma erase_qualifiers_idemp t : erase_qualifiers (erase_qualifiers t) = erase_qualifiers t.
+Proof.
+  move: t. fix IHt 1=>t.
+  destruct t as [| | | | | | | | |cc ar ret args| | | | | |]; cbn; auto with f_equal.
+  { (* functions *) rewrite IHt. f_equal. induction args; cbn; auto with f_equal. }
+Qed.
+
+Lemma drop_erase_qualifiers t : drop_qualifiers (erase_qualifiers t) = erase_qualifiers t.
+Proof. by rewrite drop_qualifiers_unqual. Qed.
+Lemma erase_drop_qualifiers t : erase_qualifiers (drop_qualifiers t) = erase_qualifiers t.
+Proof. induction t; cbn; auto. Qed.
+
+#[deprecated(since="20230531", note="Use [drop_erase_qualifiers]")]
+Notation drop_erase := drop_erase_qualifiers.
+#[deprecated(since="20230531", note="Use [erase_drop_qualifiers]")]
+Notation erase_drop := drop_erase_qualifiers.
+
+Lemma unqual_erase_qualifiers ty tq ty' : erase_qualifiers ty <> Tqualified tq ty'.
+Proof. by induction ty. Qed.
+
+(** ** Qualifier simplification tactic *)
+(* Lemmas for all [type] constructors; in constructor order for easy review. *)
+Lemma drop_qualifiers_Tptr : forall [ty ty'],
+    drop_qualifiers ty = Tptr ty' -> erase_qualifiers ty = Tptr (erase_qualifiers ty').
+Proof. induction ty; simpl; intros; try congruence; eauto. Qed.
+Lemma drop_qualifiers_Tref : forall [ty ty'],
+    drop_qualifiers ty = Tref ty' -> erase_qualifiers ty = Tref (erase_qualifiers ty').
+Proof. induction ty; simpl; intros; try congruence; eauto. Qed.
+Lemma drop_qualifiers_Trv_ref : forall [ty ty'],
+    drop_qualifiers ty = Trv_ref ty' -> erase_qualifiers ty = Trv_ref (erase_qualifiers ty').
+Proof. induction ty; simpl; intros; try congruence; eauto. Qed.
+Lemma drop_qualifiers_Tnum : forall [ty sz sgn],
+    drop_qualifiers ty = Tnum sz sgn -> erase_qualifiers ty = Tnum sz sgn.
+Proof. by induction ty. Qed.
+Lemma drop_qualifiers_Tchar_ : forall [ty ct],
+    drop_qualifiers ty = Tchar_ ct -> erase_qualifiers ty = Tchar_ ct.
+Proof. by induction ty. Qed.
+Lemma drop_qualifiers_Tvoid : forall [ty],
+    drop_qualifiers ty = Tvoid -> erase_qualifiers ty = Tvoid.
+Proof. induction ty; simpl; intros; try congruence; eauto. Qed.
+Lemma drop_qualifiers_Tarray : forall [ty ty' n],
+    drop_qualifiers ty = Tarray ty' n -> erase_qualifiers ty = Tarray (erase_qualifiers ty') n.
+Proof. induction ty; simpl; intros; try congruence; eauto. Qed.
+Lemma drop_qualifiers_Tnamed : forall [ty n],
+    drop_qualifiers ty = Tnamed n -> erase_qualifiers ty = Tnamed n.
+Proof. induction ty; simpl; intros; try congruence; eauto. Qed.
+Lemma drop_qualifiers_Tenum : forall [ty nm],
+    drop_qualifiers ty = Tenum nm -> erase_qualifiers ty = Tenum nm.
+Proof. induction ty; simpl; intros; try congruence; eauto. Qed.
+Lemma drop_qualifiers_Tfunction : forall [ty c ar ty' tArgs],
+    drop_qualifiers ty = @Tfunction c ar ty' tArgs ->
+    erase_qualifiers ty = @Tfunction c ar (erase_qualifiers ty') (map erase_qualifiers tArgs).
+Proof. induction ty; simpl; intros; try congruence; eauto. Qed.
+Lemma drop_qualifiers_Tbool : forall [ty],
+    drop_qualifiers ty = Tbool -> erase_qualifiers ty = Tbool.
+Proof. induction ty; simpl; intros; try congruence; eauto. Qed.
+Lemma drop_qualifiers_Tmember_pointer : forall [ty cls ty'],
+    drop_qualifiers ty = Tmember_pointer cls ty' ->
+    erase_qualifiers ty = Tmember_pointer cls (erase_qualifiers ty').
+Proof. induction ty; simpl; intros; try congruence; eauto. Qed.
+Lemma drop_qualifiers_Tfloat : forall [ty sz],
+    drop_qualifiers ty = Tfloat_ sz -> erase_qualifiers ty = Tfloat_ sz.
+Proof. induction ty; simpl; intros; try congruence; eauto. Qed.
+(* Omit Tqualified on purpose *)
+Lemma drop_qualifiers_Tnullptr : forall [ty],
+    drop_qualifiers ty = Tnullptr -> erase_qualifiers ty = Tnullptr.
+Proof. induction ty; simpl; intros; try congruence; eauto. Qed.
+
+(** simplify instances where you have [drop_qualifiers ty = Txxx ..] for some [Txxx]. *)
+(* Same order as above, for easier review. *)
+Ltac simpl_drop_qualifiers :=
+  match goal with
+  | H : drop_qualifiers _ = _ |- _ => first
+          [ rewrite (drop_qualifiers_Tptr H)
+          | rewrite (drop_qualifiers_Tref H)
+          | rewrite (drop_qualifiers_Trv_ref H)
+          | rewrite (drop_qualifiers_Tnum H)
+          | rewrite (drop_qualifiers_Tchar_ H)
+          | rewrite (drop_qualifiers_Tvoid H)
+          | rewrite (drop_qualifiers_Tarray H)
+          | rewrite (drop_qualifiers_Tnamed H)
+          | rewrite (drop_qualifiers_Tenum H)
+          | rewrite (drop_qualifiers_Tfunction H)
+          | rewrite (drop_qualifiers_Tbool H)
+          | rewrite (drop_qualifiers_Tmember_pointer H)
+          | rewrite (drop_qualifiers_Tfloat H)
+          | rewrite (drop_qualifiers_Tnullptr H)
+          ]
+  end.
+
+(** ** Smart constructors *)
 
 (**
 Qualify a type, merging nested qualifiers, suppressing [QM]
@@ -815,6 +986,9 @@ Lemma is_ref_spec t : is_ref t <-> ∃ t', t = Tref t' \/ t = Trv_ref t'.
 Proof. split=>Href. by destruct t; eauto. by destruct Href as (?&[-> | ->]). Qed.
 
 Lemma is_ref_unqualified t : is_ref t -> ~~ is_qualified t.
+Proof. by destruct t. Qed.
+
+Lemma is_ref_drop_qualifiers t : is_ref t -> is_ref (drop_qualifiers t).
 Proof. by destruct t. Qed.
 
 (**
@@ -908,6 +1082,22 @@ Proof.
   - done.
 Qed.
 
+Lemma drop_qualifiers_tqualified q t :
+  drop_qualifiers (tqualified q t) = drop_qualifiers t.
+Proof.
+  induction (tqualified_ok q t).
+  { by destruct (tqualified'_ok q t). }
+  { done. }
+Qed.
+
+Lemma erase_qualifiers_tqualified q t :
+  erase_qualifiers (tqualified q t) = erase_qualifiers t.
+Proof.
+  induction (tqualified_ok q t).
+  { by destruct (tqualified'_ok q t). }
+  { done. }
+Qed.
+
 (** [tref] *)
 
 Inductive tref_spec : type_qualifiers -> type -> type -> Prop :=
@@ -985,6 +1175,7 @@ Lemma trv_ref_unfold q t :
     ) q t.
 Proof. move: q. by induction t; cbn. Qed.
 
+(** ** Type normalization *)
 (**
 normalization of types
 - compresses adjacent [Tqualified] constructors
@@ -1063,6 +1254,223 @@ End normalize_type_idempotent.
 Lemma normalize_type_qual_norm t :
   normalize_type t = qual_norm (fun q t' => tqualified q (normalize_type t')) t.
 Proof. rewrite qual_norm_unfold. by destruct t. Qed.
+
+(** ** Qualifier-aware type operations *)
+
+(**
+[unptr t] returns the type of the object that a value of type [t]
+points to or [None] if [t] is not a pointer type.
+*)
+Definition unptr (t : type) : option type :=
+  match drop_qualifiers t with
+  | Tptr p => Some p
+  | _ => None
+  end.
+
+(**
+[drop_reference t] removes any leading reference types.
+*)
+Fixpoint drop_reference (t : type) : exprtype :=
+  match drop_qualifiers t with
+  | Tref u | Trv_ref u => drop_reference u
+  | _ => t	(** We do not normalize qualifiers here to promote sharing *)
+  end.
+
+(**
+[class_name t] returns the name of the class that this type refers to
+*)
+Definition class_name (t : type) : option globname :=
+  match drop_qualifiers t with
+  | Tnamed gn => Some gn
+  | _ => None
+  end.
+
+(**
+[is_arithmetic ty] states whether [ty] is an arithmetic type
+*)
+Definition is_arithmetic (ty : type) : bool :=
+  match drop_qualifiers ty with
+  | Tnum _ _
+  | Tchar_ _
+  | Tenum _
+  | Tbool => true
+  | _ => false
+  end.
+
+(**
+[is_pointer ty] is [true] if [ty] is a pointer type
+*)
+Definition is_pointer (ty : type) : bool :=
+  match drop_qualifiers ty with
+  | Tptr _ => true
+  | _ => false
+  end.
+
+Lemma is_pointer_not_arithmetic : forall ty, is_pointer ty = true -> is_arithmetic ty = false.
+Proof. induction ty; simpl; intros; eauto. Qed.
+Lemma is_arithmetic_not_pointer : forall ty, is_arithmetic ty = true -> is_pointer ty = false.
+Proof. induction ty; simpl; intros; eauto. Qed.
+
+(**
+Formalizes https://eel.is/c++draft/basic.types.general#term.scalar.type.
+*)
+Definition scalar_type (ty : type) : bool :=
+  match drop_qualifiers ty with
+  | Tnullptr | Tptr _
+  | Tmember_pointer _ _
+  | Tfloat_ _
+  | Tchar_ _
+  | Tbool
+  | Tnum _ _ | Tenum _ => true
+  | _ => false
+  end.
+Lemma scalar_type_erase_drop ty :
+  scalar_type (erase_qualifiers ty) = scalar_type (drop_qualifiers ty).
+Proof. by induction ty. Qed.
+
+(**
+[is_value_type t] returns [true] if [t] has value semantics. A value
+type is one that can be represented by [val].
+
+NOTE: The only difference between a value type and a scalar type is
+that [Tvoid] is a value type and not a scalar type.
+ *)
+Definition is_value_type (t : type) : bool :=
+  match drop_qualifiers t with
+  | Tnum _ _
+  | Tchar_ _
+  | Tbool
+  | Tptr _
+  | Tnullptr
+  | Tfloat_ _
+  | Tmember_pointer _ _
+  (**
+  NOTE: In C++ the the underlying type of an enumeration must be an
+  integral type. This definition presuppposes [t] a valid enumeration.
+  *)
+  | Tenum _ (* enum types are value types *)
+  | Tvoid => true
+  | _ => false
+  end.
+
+Lemma is_value_type_erase_qualifiers ty :
+  is_value_type (erase_qualifiers ty) = is_value_type ty.
+Proof. induction ty; cbn; auto. Qed.
+Lemma is_value_type_drop_qualifiers ty :
+  is_value_type (drop_qualifiers ty) = is_value_type ty.
+Proof. induction ty; cbn; auto. Qed.
+
+Lemma is_value_type_qual_norm' q t :
+  is_value_type t = qual_norm' (fun _ t' => is_value_type t') q t.
+Proof. by elim: (qual_norm'_ok _ q t). Qed.
+Lemma is_value_type_qual_norm t :
+  is_value_type t = qual_norm (fun _ t' => is_value_type t') t.
+Proof. apply is_value_type_qual_norm'. Qed.
+Lemma is_value_type_decompose_type t :
+  is_value_type t = is_value_type (decompose_type t).2.
+Proof. by rewrite is_value_type_qual_norm qual_norm_decompose_type. Qed.
+
+(**
+[is_reference_type t] returns [true] if [t] is a (possibly
+cv-qualified) reference type.
+*)
+Definition is_reference_type (t : type) : bool :=
+  is_ref (drop_qualifiers t).
+
+Lemma is_ref_incl t : is_ref t -> is_reference_type t.
+Proof. apply is_ref_drop_qualifiers. Qed.
+
+Lemma value_type_non_ref {ty} : is_value_type ty -> ~~ is_reference_type ty.
+Proof. by induction ty. Qed.
+
+Lemma is_reference_type_erase_qualifiers t :
+  is_reference_type (erase_qualifiers t) = is_reference_type t.
+Proof. induction t; cbn; auto. Qed.
+Lemma is_reference_type_drop_qualifiers t :
+  is_reference_type (drop_qualifiers t) = is_reference_type t.
+Proof. induction t; cbn; auto. Qed.
+
+Lemma is_reference_type_qual_norm' q t :
+  is_reference_type t = qual_norm' (fun _ t => is_reference_type t) q t.
+Proof. by elim: (qual_norm'_ok _ q t). Qed.
+Lemma is_reference_type_qual_norm t :
+  is_reference_type t = qual_norm (fun _ t => is_reference_type t) t.
+Proof. apply is_reference_type_qual_norm'. Qed.
+Lemma is_reference_type_decompose_type t :
+  is_reference_type t = is_reference_type (decompose_type t).2.
+Proof. by rewrite is_reference_type_qual_norm qual_norm_decompose_type. Qed.
+
+(**
+[as_ref' f x t] applies [f u] if [t] is a (possibly cv-qualified)
+reference type with underlying type [u], defaulting to [x] if [t] is
+not a reference type.
+
+The special cases [as_ref t : type] and [as_ref_option : option type]
+return the underlying type [u] (defaulting, respectively, to a dummy
+type and to [None]).
+*)
+
+Definition as_ref' {A} (f : exprtype -> A) (x : A) (t : type) : A :=
+  if drop_qualifiers t is (Tref u | Trv_ref u) then f u else x.
+Notation as_ref := (as_ref' (fun u => u) Tvoid).
+Notation as_ref_option := (as_ref' Some None).
+
+Lemma as_ref'_erase_qualifiers {A} (f : exprtype -> A) (x : A) t :
+  as_ref' f x (erase_qualifiers t) = as_ref' (f ∘ erase_qualifiers) x t.
+Proof. induction t; cbn; auto. Qed.
+Lemma as_ref_erase_qualifiers t :
+  as_ref (erase_qualifiers t) = erase_qualifiers (as_ref t).
+Proof. induction t; cbn; auto. Qed.
+
+Section as_ref'.
+  Context {A : Type} (f : exprtype -> A) (x : A).
+  #[local] Notation as_ref' := (as_ref' f x).
+
+  Lemma as_ref_drop_qualifiers t : as_ref' (drop_qualifiers t) = as_ref' t.
+  Proof. induction t; cbn; auto. Qed.
+  Lemma as_ref_qual_norm' q t : as_ref' t = qual_norm' (fun _ t => as_ref' t) q t.
+  Proof. by elim: (qual_norm'_ok _ q t). Qed.
+  Lemma as_ref_qual_norm t : as_ref' t = qual_norm (fun _ t => as_ref' t) t.
+  Proof. apply as_ref_qual_norm'. Qed.
+  Lemma as_ref_decompose_type t : as_ref' t = as_ref' (decompose_type t).2.
+  Proof. by rewrite as_ref_qual_norm qual_norm_decompose_type. Qed.
+End as_ref'.
+
+(**
+[is_aggregate_type t] returns [true] if [t] is a (possibly qualified)
+structure or array type.
+*)
+Definition is_aggregate_type (ty : type) : bool :=
+  match drop_qualifiers ty with
+  | Tnamed _ | Tarray _ _ => true
+  | _ => false
+  end.
+
+Lemma is_aggregate_type_drop_qualifiers ty :
+  is_aggregate_type (drop_qualifiers ty) = is_aggregate_type ty.
+Proof.
+  by rewrite /is_aggregate_type drop_qualifiers_idemp.
+Qed.
+Lemma is_aggregate_type_erase_qualifiers ty :
+  is_aggregate_type (erase_qualifiers ty) = is_aggregate_type ty.
+Proof. by induction ty. Qed.
+
+Lemma is_aggregate_type_qual_norm' cv ty :
+  is_aggregate_type ty = qual_norm' (fun _ ty' => is_aggregate_type ty') cv ty.
+Proof. by elim: (qual_norm'_ok _ _ _). Qed.
+Lemma is_aggregate_type_qual_norm ty :
+  is_aggregate_type ty = qual_norm (fun _ ty' => is_aggregate_type ty')  ty.
+Proof. apply is_aggregate_type_qual_norm'. Qed.
+Lemma is_aggregate_type_decompose_type ty :
+  is_aggregate_type ty = is_aggregate_type (decompose_type ty).2.
+Proof.
+  by rewrite is_aggregate_type_qual_norm qual_norm_decompose_type.
+Qed.
+
+Lemma aggregate_type_non_ref ty : is_aggregate_type ty -> ~~ is_reference_type ty.
+Proof. by induction ty. Qed.
+Lemma aggregate_type_non_val ty : is_aggregate_type ty -> ~~ is_value_type ty.
+Proof. by induction ty. Qed.
 
 (** ** Notation for character types *)
 Coercion Tchar_ : char_type.t >-> type.
