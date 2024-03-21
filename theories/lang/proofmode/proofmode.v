5449648ca46e6026f1ec148ad634ed92aee5aa9b
Jan-Oliver Kaiser <janno@bedrocksystems.com>
2024-03-21 22:56:17 +0100

    Do not call `cbv` in `iFrame`

--- theories/lang/proofmode/proofmode.v
+++ theories/lang/proofmode/proofmode.v
@@ -22,3 +22,25 @@ Section class_instances_frame.
     Frame p R (|={E1,E2}=> P) (|={E1,E2}=> Q) | 2.
   Proof. rewrite /Frame=>/(_ ltac:(constructor))<-. by rewrite fupd_frame_l. Qed.
 End class_instances_frame.
+
+(* https://gitlab.mpi-sws.org/iris/iris/-/merge_requests/1042 *)
+Import environments.
+#[global]
+Ltac iFrameAnyIntuitionistic ::=
+  iStartProof;
+  let rec go Hs :=
+    match Hs with [] => idtac | ?H :: ?Hs => repeat iFrameHyp H; go Hs end in
+  match goal with
+  | |- envs_entails ?Δ _ =>
+     let Hs := eval lazy in (env_dom (env_intuitionistic Δ)) in go Hs
+  end.
+
+#[global]
+Ltac iFrameAnySpatial ::=
+  iStartProof;
+  let rec go Hs :=
+    match Hs with [] => idtac | ?H :: ?Hs => try iFrameHyp H; go Hs end in
+  match goal with
+  | |- envs_entails ?Δ _ =>
+     let Hs := eval lazy in (env_dom (env_spatial Δ)) in go Hs
+  end.
