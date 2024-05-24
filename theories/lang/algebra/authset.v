73cb8a5fddd8a26e49831520d1ee5ef878eeb6e6
Hai Dang <hai@bedrocksystems.com>
2024-05-24 14:49:24 +0200

    Upstream AuthSet

---
+++ theories/lang/algebra/authset.v
@@ -0,0 +1,188 @@
+(*
+ * Copyright (C) BedRock Systems Inc. 2021
+ *
+ * This software is distributed under the terms of the BedRock Open-Source License.
+ * See the LICENSE-BedRock file in the repository root for details.
+ *)
+
+Require Import stdpp.base.
+Require Import stdpp.sets.
+Require Import stdpp.propset.
+Require Export iris.algebra.cmra.
+Require Export iris.algebra.excl.
+Require Export iris.algebra.local_updates.
+Require Export iris.algebra.auth.
+
+Require Import iris.base_logic.base_logic.
+Require Import bedrock.lang.algebra.restrict.
+
+Set Default Proof Using "Type".
+
+(** * CMRA: Auth/frag splits on sets, with strengthening
+
+  This CMRA exposes:
+    * AuthSet.frag (s : propset A)
+    * AuthSet.auth (s : propset A)
+  such that
+    * ✓ (AuthSet.frag s1 ⋅ AuthSet.auth s2) <-> s1 ⊆ s2; and
+    * AuthSet.frag s ~~> AuthSet.frag s' when s' ⊆ s (removing elements
+      from a frag set is frame preserving).
+  The CMRA is built using the `restrict.v` framework.
+  This CMRA is used in ``lang/base_logic/lib/auth_set.v``.
+*)
+
+Canonical Structure propsetO (A : Type) : ofe := discreteO (propset A).
+
+Notation _auth_setT A :=
+  (prodR (optionR (exclR (propsetO A))) (optionR (exclR (propsetO A))))
+  (only parsing).
+Variant AuthSet : Type :=.
+#[local] Definition auth_set' (A : Type) :=
+  wrapper AuthSet (_auth_setT A).
+Notation auth_set A := (Reduce (auth_set' A)) (only parsing).
+
+Section with_Type.
+  Context `{A : Type, Countable A}.
+  Implicit Types x y : _auth_setT A.
+  Definition auth_set_Restrictor x :=
+    forall s1 s2, fst x ≡ Excl' s1 -> snd x ≡ Excl' s2 -> s1 ⊆ s2.
+
+  #[local] Lemma auth_set_cmra_restrict_ne n :
+    Proper (dist n ==> impl) auth_set_Restrictor.
+  Proof.
+    case => a b; case => c d; case => /=; rewrite -2!discrete_iff => h1 h2.
+      by move => h3 l1 l; rewrite -h1 -h2 => x y; apply: h3.
+  Qed.
+  #[local] Lemma auth_set_cmra_restrict_op_l n (x y : _auth_setT A) :
+    ✓{n} (x ⋅ y) ->
+    auth_set_Restrictor (x ⋅ y) → auth_set_Restrictor x.
+  Proof.
+    case: x => a b; case: y => c d /=; case => /= x y h1 l1 l /=.
+    move => h2 h3; apply: h1; first by move: x; rewrite h2; case: c.
+      by move: y; rewrite h3; case: d.
+  Qed.
+
+  #[global] Instance auth_set_Restriction :
+    Restriction AuthSet (_auth_setT A) :=
+    Build_Restriction AuthSet (_auth_setT A) auth_set_Restrictor
+                      auth_set_cmra_restrict_ne
+                      auth_set_cmra_restrict_op_l.
+End with_Type.
+
+Canonical Structure auth_setR (A : Type) :=
+  restrictR AuthSet (_auth_setT A).
+
+Module AuthSet.
+  Definition frag_def {A} (s1 : propset A) : auth_set A :=
+    Wrap (Excl' s1, None).
+  Definition frag_aux : seal (@frag_def). Proof. by eexists. Qed.
+  Definition frag := frag_aux.(unseal).
+  Definition frag_eq : @frag = _ := frag_aux.(seal_eq).
+
+  Arguments frag {_} _.
+
+  Definition auth_def {A} (s2 : propset A) : auth_set A :=
+    Wrap (None, Excl' s2).
+  Definition auth_aux : seal (@auth_def). Proof. by eexists. Qed.
+  Definition auth := auth_aux.(unseal).
+  Definition auth_eq : @auth = _ := auth_aux.(seal_eq).
+
+  Arguments auth {_} _.
+End AuthSet.
+
+Lemma auth_set_validN_valid {A} n (x : auth_setR A) : ✓{n} x <-> ✓x.
+Proof. by rewrite restrict_validN restrict_valid. Qed.
+
+Lemma auth_set_valid_frag {A} (s1 : propset A) : ✓ AuthSet.frag s1.
+Proof.
+  rewrite AuthSet.frag_eq restrict_valid; split => //.
+  move => s1' l /=. do 2 inversion 1.
+Qed.
+
+Lemma auth_set_valid_auth {A} (l : propset A) : ✓ AuthSet.auth l.
+Proof.
+  rewrite AuthSet.auth_eq restrict_valid; split => //.
+  move => s1 l' /=. do 2 inversion 1.
+Qed.
+
+Lemma auth_set_auth_excl {A} (s1 s2 : propset A) :
+  ~ ✓ (AuthSet.auth s1 ⋅ AuthSet.auth s2).
+Proof.
+  rewrite AuthSet.auth_eq restrict_valid => []/=[]/=.
+  inversion 1; simpl in *.
+  match goal with H : ✓ (Excl' _ ⋅ Excl' _) |- _ => inversion H end.
+Qed.
+
+Lemma auth_set_frag_excl {A} (s1 s2 : propset A) :
+  ~ ✓ (AuthSet.frag s1 ⋅ AuthSet.frag s2).
+Proof.
+  rewrite AuthSet.frag_eq restrict_valid => []/=[]/=.
+  inversion 1; simpl in *.
+  match goal with H : ✓ (Excl' _ ⋅ Excl' _) |- _ => inversion H end.
+Qed.
+
+Lemma auth_set_valid_frag_auth {A} (s1 s2 : propset A) :
+  ✓ (AuthSet.frag s1 ⋅ AuthSet.auth s2) <-> s1 ⊆ s2.
+Proof.
+  rewrite AuthSet.frag_eq AuthSet.auth_eq; split.
+  { by rewrite restrict_valid; case => /=; case => /= _ _; apply. }
+  move => h; rewrite restrict_valid; split => // s1' l' /=.
+  inversion 1. inversion 1. subst. inversion H2; subst. inversion H6; subst.
+    by rewrite <-H4, <-H5.
+Qed.
+
+(** ** Removing elements from a frag set is frame preserving. *)
+Lemma auth_set_update_frag {A} (s' s : propset A) (_ : s' ⊆ s):
+  AuthSet.frag s ~~> AuthSet.frag s'.
+Proof.
+  move => n; case => x /=; last by apply: auth_set_valid_frag.
+  case: x; case => a b; rewrite restrict_validN /=; case.
+  rewrite AuthSet.frag_eq /=; case: a => //= X Y; split.
+  { split => //=; rewrite /op/cmra_op/=; destruct b => //.
+    rewrite Some_validN.
+    by rewrite /valid/cmra_valid/=; case: X => /=; case: c Y. }
+  simpl => x y /=. inversion 1. inversion 1. subst.
+  have h2: b ≡ Excl' y.
+  { clear X Y; case: b H4 H5; last by inversion 1.
+    move => a; inversion 1; subst. inversion 1; subst. by rewrite H5. }
+  have H2: s ⊆ y by apply: Y.
+  inversion H3; subst. rewrite <-H8. clear - H H2. set_solver.
+Qed.
+
+Lemma auth_set_update {A} (s1' l' s1 l : propset A) (_ : s1' ⊆ l') :
+  AuthSet.frag s1 ⋅ AuthSet.auth l ~~>
+  AuthSet.frag s1' ⋅ AuthSet.auth l'.
+Proof.
+  move => n; case => x /=; last first.
+  { by rewrite -cmra_discrete_valid_iff auth_set_valid_frag_auth. }
+  case: x => [][]a b.
+  rewrite restrict_validN; case.
+  rewrite AuthSet.auth_eq AuthSet.frag_eq /=; case => /= h1 h2.
+  have ->: a = None by case: a h1.
+  have ->: b = None by case: b h2. move => h3.
+  rewrite /AuthSet.frag_def /AuthSet.auth_def restrict_validN /=.
+  split => // lx ly /=. inversion 1; subst. inversion 1; subst.
+  inversion H3; subst.
+  inversion H5; subst. rewrite -H6 -H7. apply: H.
+Qed.
+
+#[global] Instance frag_proper {A} : Proper (equiv ==> equiv) (AuthSet.frag (A:=A)).
+Proof.
+  move => x y E; rewrite AuthSet.frag_eq/AuthSet.frag_def.
+  apply: Wrap_Proper; apply: pair_proper; last by [].
+  apply: Some_proper; apply: Excl_proper; apply: E.
+Qed.
+
+#[global] Instance auth_proper {A} : Proper (equiv ==> equiv) (AuthSet.auth (A:=A)).
+Proof.
+  move => x y E; rewrite AuthSet.auth_eq/AuthSet.auth_def.
+  apply: Wrap_Proper; apply: pair_proper; first by [].
+  apply: Some_proper; apply: Excl_proper; apply: E.
+Qed.
+
+#[local] Lemma auth_set_test :
+  ✓ (AuthSet.frag {[ 1%N ]} ⋅ AuthSet.auth ({[ 1%N ]} ∪ {[ 2%N ]})).
+Proof.
+  rewrite auth_set_valid_frag_auth.
+  set_solver.
+Fail Fail Qed. Abort. (*NOTE: to avoid polluting search, or is #[local] enough?*)
---
+++ theories/lang/algebra/restrict.v
@@ -0,0 +1,248 @@
+(*
+ * Copyright (C) BedRock Systems Inc. 2020
+ *
+ * This software is distributed under the terms of the BedRock Open-Source License.
+ * See the LICENSE-BedRock file in the repository root for details.
+ *)
+
+Require Import stdpp.list.
+Require Export iris.algebra.cmra.
+Require Export iris.algebra.excl.
+Require Export iris.algebra.local_updates.
+Require Export iris.algebra.auth.
+Require Export bedrock.prelude.base.
+
+#[local] Definition resource' (A : Type) : cmra := exclR (leibnizO A).
+#[global] Notation resource A := (Reduce (resource' A)) (only parsing).
+
+(* Type-indexed wrappers, used in the restrict cmra below *)
+Record wrapper (I A : Type) := Wrap { unwrap : A }.
+Add Printing Constructor wrapper.
+Arguments Wrap {I A} _.
+Arguments unwrap {I A} _.
+#[global] Instance wrapper_equiv I A `{Equiv A} : Equiv (wrapper I A) :=
+  fun x y => x.(unwrap) ≡ y.(unwrap).
+#[global] Instance Wrap_Proper I A `{Equiv A} : Proper (equiv ==> equiv) (@Wrap I A).
+Proof. by move => x y h. Qed.
+#[global] Instance unwrap_Proper I A `{Equiv A} : Proper (equiv ==> equiv) (@unwrap I A).
+Proof. by move => x y h. Qed.
+#[global] Instance wrapper_Equivalence {I : Type} `{Equiv A} `{X : @Equivalence _ (≡@{A})} :
+  Equivalence (≡@{wrapper I A}).
+Proof.
+  case: X => h1 h2 h3; split.
+  { case => x; apply: h1. }
+  { by case => x; case => y h4; apply: h2. }
+  case => x; case => y; case => z; apply: h3.
+Qed.
+Lemma Wrap_eq_iff I `{X : Equiv A} (a b : A) :
+  @Wrap I A a ≡ @Wrap I A b <-> a ≡ b.
+Proof.
+  split.
+  - by rewrite /equiv/wrapper_equiv.
+  - by rewrite /equiv/wrapper_equiv.
+Qed.
+#[global] Instance wrapper_LeibnizEquiv {I : Type} `{Equiv A} `{X : @LeibnizEquiv _ (≡@{A})} :
+  @LeibnizEquiv _ (≡@{wrapper I A}).
+Proof.
+  case => x; case => y h; f_equal; apply: X.
+  by rewrite -Wrap_eq_iff.
+Qed.
+#[global] Instance Wrap_inj I `{X : Equiv A} : Inj (≡@{A}) (≡@{wrapper I A}) Wrap.
+Proof. by move => x y; rewrite Wrap_eq_iff. Qed.
+#[global] Instance unwrap_inj I `{Equiv A, !Equivalence (≡@{A})}
+  : Inj (≡@{wrapper I A}) (≡@{A}) unwrap.
+Proof. by case => x; case => y /= ->. Qed.
+
+Section with_ofeT.
+Context {I : Type} {A : ofe}.
+Implicit Types x y : wrapper I A.
+
+#[local] Instance wrapper_Dist : Dist (wrapper I A) :=
+  fun n x y => x.(unwrap) ≡{n}≡ y.(unwrap).
+Lemma wrapper_dist n x y : x ≡{n}≡ y <-> x.(unwrap) ≡{n}≡ y.(unwrap).
+Proof. by []. Qed.
+Lemma Wrap_dist_iff n (a b : A) : @Wrap I A a ≡{n}≡ @Wrap I A b <-> a ≡{n}≡ b.
+Proof.
+  split.
+  - by rewrite /equiv/wrapper_dist.
+  - by rewrite /equiv/wrapper_dist.
+Qed.
+Lemma wrapper_ofe_mixin : OfeMixin (wrapper I A).
+Proof.
+  constructor.
+  - case => x; case => y; rewrite Wrap_eq_iff; split.
+    { by move => h n; rewrite /dist/wrapper_Dist/= h. }
+    rewrite /dist/wrapper_Dist/=.
+    rewrite -mixin_equiv_dist => //. apply: ofe_mixin.
+  - move => n; split => //.
+    { rewrite /dist/wrapper_Dist/= => x //. }
+    { rewrite /dist/wrapper_Dist/= => x y //. }
+    { by rewrite /dist/wrapper_Dist/= => x y z // -> ->. }
+  - move => n m [x] [y].
+    rewrite /dist/wrapper_Dist/=.
+    apply: mixin_dist_lt. apply: ofe_mixin.
+Qed.
+Canonical Structure wrapperO := Ofe _ wrapper_ofe_mixin.
+End with_ofeT.
+
+Section ofe_properties.
+  Context {I : Type} {A : ofe}.
+
+  #[global] Instance Wrap_ne : NonExpansive (@Wrap I A).
+  Proof. solve_proper. Qed.
+  #[global] Instance unwrap_ne : NonExpansive (@unwrap I A).
+  Proof. solve_proper. Qed.
+
+  #[global] Instance Wrap_inj_dist n : Inj (dist n) (dist n) (@Wrap I A).
+  Proof. by move=>a b. Qed.
+  #[global] Instance unwrap_inj_dist n : Inj (dist n) (dist n) (@unwrap I A).
+  Proof. by move=>a b. Qed.
+End ofe_properties.
+
+(* Restriction cmra (derived from @swasey's restrict.v)*)
+
+(*jgs: The operational typeclass approach causes prefix_test to fail
+(missing Valid instance). I don't yet understand why.
+Class CmraRestrictor (I A : Type) : Type := restrict : A -> Prop.
+Class Restriction I (A : cmra) `{CmraRestrictor I A} :=
+{ cmra_restrict_ne n : Proper (dist n ==> impl) restrict
+; cmra_restrict_op_l n (x y : wrapper I A) :
+    ✓{n} (x ⋅ y).(unwrap) →
+    restrict (x ⋅ y).(unwrap) →
+    restrict x.(unwrap) }.*)
+Class Restriction (I : Type) (A : cmra) :=
+{ restrict : A -> Prop
+; cmra_restrict_ne n : Proper (dist n ==> impl) restrict
+; cmra_restrict_op_l n (x y : A) :
+    ✓{n} (x ⋅ y) ->
+    restrict (x ⋅ y) ->
+    restrict x }.
+
+Section with_Restriction.
+Variables (I : Type) (A : cmra).
+Context `{Restriction I A}.
+Instance restrict_valid_def : Valid (wrapper I A) :=
+  λ x, ✓ x.(unwrap) ∧ restrict x.(unwrap).
+Instance restrict_validN_def : ValidN (wrapper I A) :=
+  λ n x, ✓{n} x.(unwrap) ∧ restrict x.(unwrap).
+Notation "✓{ n }@{ A } x" := (@validN _ (cmra_validN A) n x)
+  (at level 20, n at next level, only parsing) : stdpp_scope.
+Notation "✓@{ A } x" := (@valid _ (cmra_valid A) x)
+  (at level 20, only parsing) : stdpp_scope.
+Lemma restrict_valid (x : wrapper I A) :
+  ✓ x ↔ ✓@{A} x.(unwrap) ∧ restrict x.(unwrap).
+Proof. by done. Qed.
+Lemma restrict_validN n (x : wrapper I A) :
+  ✓{n} x ↔ ✓{n}@{A} x.(unwrap) ∧ restrict x.(unwrap).
+Proof. done. Qed.
+
+#[local] Instance wrapper_PCore : PCore (wrapper I A) :=
+  fun x => Wrap <$> pcore x.(unwrap).
+Lemma wrapper_pcore x : pcore x = Wrap <$> pcore x.(unwrap).
+Proof. by []. Qed.
+#[local] Instance wrapper_Op : Op (wrapper I A) :=
+  fun x y => Wrap $ x.(unwrap) ⋅ y.(unwrap).
+Lemma wrapper_op x : op x = fun y => Wrap $ x.(unwrap) ⋅ y.(unwrap).
+Proof. by []. Qed.
+
+Definition restrict_cmra_mixin : CmraMixin (wrapper I A).
+Proof.
+  split.
+  - case => x /=; rewrite wrapper_op => n a b h /=.
+    by apply: cmra_op_ne.
+  - move => n; case => x; case => y; case => cx.
+    rewrite wrapper_dist /= => h1 h2.
+    case: (cmra_pcore_ne _ _ _ cx h1).
+    { move: h2; rewrite wrapper_pcore /=.
+      case: (cmra_pcore A x) => // [a|].
+      { by case: (pcore x) => //= z; inversion 1; subst. }
+      by case: (pcore x) => //= z; inversion 1; subst. }
+    move => cy []h3 h4; exists (Wrap cy); split => //.
+    move: h2; rewrite wrapper_pcore /=; case: (pcore x) => // z.
+    by inversion 1; subst; rewrite wrapper_pcore /= h3.
+  - intros n x1 x2 Hx []. split.
+    exact: cmra_validN_ne. exact: cmra_restrict_ne.
+  - case => x. rewrite restrict_valid /= cmra_valid_validN.
+    setoid_rewrite restrict_validN. naive_solver eauto using O.
+  - intros n x []. split. exact: cmra_validN_S. done.
+  - case => x; case => y; case => z; apply: cmra_assoc.
+  - case => x; case => y; apply: cmra_comm.
+  - case => x; case => cx; rewrite /pcore/wrapper_PCore/= => h.
+    rewrite /op/wrapper_Op/=; move: (cmra_pcore_l x cx); case: (pcore x) h => //.
+    move => a; inversion 1; subst; move/(_ eq_refl) => h1.
+    by rewrite Wrap_eq_iff.
+  - case => x; case => cx; rewrite /pcore/wrapper_PCore/= // => -.
+    move: (cmra_pcore_idemp x cx); case: (pcore x) => // a /= h1; inversion 1; subst.
+    move: (h1 eq_refl); case: (pcore cx) => /=.
+    { move => a h2. f_equiv. move/Some_equiv_eq: h2 => [?] [[-> ->]] //. }
+    rewrite !option_equiv_Forall2; inversion 1.
+  - case => x; case => y; case => cx.
+    case; case => a; rewrite /op/wrapper_Op/=.
+    rewrite /pcore/wrapper_PCore/= Wrap_eq_iff => h1 h2.
+    case: (cmra_pcore_mono x y cx); first by exists a.
+    { by move: h2; case: (pcore x) => // ax /=; inversion 1; subst. }
+    move => a' []h3 h4; exists (Wrap a'); split => //.
+    { move: h3; case: (pcore y) => // b'; inversion 1; subst => //. }
+    case: h4 => b' h4; exists (Wrap b'); rewrite /op /= Wrap_eq_iff.
+    by rewrite h4.
+  - move => n; case => x; case => y [] /= h h1.
+    rewrite restrict_validN; split => /=; first by apply: cmra_validN_op_l.
+    move: h1.
+    have ->: x = (@Wrap I A x).(unwrap) by [].
+    have ->: y = (@Wrap I A y).(unwrap) by [].
+    move => h1. apply: cmra_restrict_op_l => //=.
+  - move => n; case => x; case => y1; case => y2 /=.
+    rewrite /validN/restrict_validN_def /=; case => h1 h2.
+    rewrite /op/wrapper_op /= /dist/wrapper_dist /= => h3.
+    case: (mixin_cmra_extend _ (cmra_mixin A) n x y1 y2 h1 h3) => z1 []z2 [] h4 []h5 h6.
+    exists (Wrap z1), (Wrap z2). split => //.
+Qed.
+
+Canonical Structure restrictR :=
+  Cmra' _ (@wrapper_ofe_mixin I A) restrict_cmra_mixin.
+End with_Restriction.
+
+#[global] Instance wrapper_OfeDiscrete I `{X : OfeDiscrete A} : OfeDiscrete (@wrapperO I A).
+Proof. by move => []x []y; rewrite wrapper_dist /=; move/(X _) => ->. Qed.
+
+Section with_Restriction.
+  Context {I : Type} `{rx : Restriction I A} {r : restrictR I A}.
+  Implicit Types x y : A.
+  Implicit Types a b : restrictR I A.
+
+  #[global] Instance restrict_CmraDiscrete {X : CmraDiscrete A} :
+    CmraDiscrete (restrictR I A).
+  Proof.
+    case: X => h1 h2; constructor; first by apply: wrapper_OfeDiscrete.
+    move => []x; rewrite restrict_validN /= => [][]h3 h4.
+    by rewrite restrict_valid; split => //=; apply: h2.
+  Qed.
+
+  (** Updates *)
+  Lemma restrict_update x y :
+    (∀ mf, restrict (x ⋅? mf) → restrict (y ⋅? mf)) →
+    x ~~> y → Wrap x ~~> Wrap y.
+  Proof.
+    move=>HP Hup n mz [X Y]. split.
+    { case: mz X Y => //=.
+      { move => a h1 h2; apply: (Hup n (Some $ unwrap a) h1). }
+      by move => h1 h2; move: (HP None h2) => /= h3; apply: (Hup n None h1). }
+    case: mz X Y => /=.
+    { move => a h1 h2; move: (Hup n (Some $ unwrap a) h1) => h3.
+      by apply: (HP (Some $ unwrap a)). }
+    move => h1 h2; apply: (HP None h2).
+  Qed.
+
+  (** Local updates *)
+  Lemma restrict_local_update x y x' y' :
+    (restrict x → restrict x') →
+    (x, y) ~l~> (x', y') → (Wrap x, Wrap y) ~l~> (Wrap x', Wrap y').
+  Proof.
+    move=>X Hup n mf /= [Y Z] Hx; split.
+    { rewrite restrict_validN; split => /=; last by apply: X.
+      case: mf Hx; first by move => a /= Hx; case: (Hup n (Some $ unwrap a)).
+      by rewrite /= => h1; case: (Hup n None). }
+    case: mf Hx; first by move => a /= Hx; case: (Hup n (Some $ unwrap a)).
+    by rewrite /= => h1; case: (Hup n None).
+  Qed.
+End with_Restriction.
