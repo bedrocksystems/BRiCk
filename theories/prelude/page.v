3c48382f7529205b4c61f9550df34c329126bafc
Hai Dang <dhhai.uns@gmail.com>
2024-09-16 09:39:25 +0200

    Upstream some definitions of page

--- theories/prelude/page.v
+++ theories/prelude/page.v
@@ -5,6 +5,71 @@
  *)
 Require Import bedrock.prelude.base.
 Require Import bedrock.prelude.addr.
+Require Import bedrock.prelude.numbers.
+
+(** 4KB pages are addressed by 12 bits *)
+Definition PAGE_BITS : N  := 12.
+Definition PAGE_SIZE : N := 2 ^ 12.
+
+Theorem PAGE_bits_size : PAGE_SIZE = (2 ^ PAGE_BITS)%N.
+Proof. reflexivity. Qed.
+
+Definition page_align_dn (addr : N) : N := Z.to_N $ align_dn addr PAGE_BITS.
+Lemma page_align_dn_below addr : (page_align_dn addr <= addr)%N.
+Proof.
+  have ?: (align_dn addr PAGE_BITS <= addr)%Z.
+  { exact: align_dn_below. }
+  rewrite /page_align_dn; lia.
+Qed.
+
+Lemma page_align_dn_mod addr :
+  (page_align_dn addr mod PAGE_SIZE = 0)%N.
+Proof.
+  rewrite -(N2Z.id PAGE_SIZE) /page_align_dn PAGE_bits_size.
+  apply N.Lcm0.mod_divide, Z2N_inj_divide, align_dn_divide; try lia.
+  apply /align_dn_nonneg; lia.
+Qed.
+
+Definition page_align_up (addr : N) := Z.to_N $ align_up addr PAGE_BITS.
+#[global] Notation page_aligned addr :=
+    (N.eqb 0 (addr mod PAGE_SIZE)) (only parsing).
+
+Lemma page_align_up_mod addr :
+  (page_align_up addr mod PAGE_SIZE = 0)%N.
+Proof.
+  rewrite -(N2Z.id PAGE_SIZE) /page_align_up PAGE_bits_size.
+  apply N.Lcm0.mod_divide, Z2N_inj_divide, align_up_divide; try lia.
+  apply /align_up_nonneg; lia.
+Qed.
+
+Lemma page_align_up_below addr : (addr <= page_align_up addr)%N.
+Proof.
+  have ?: (addr <= numbers.align_up addr PAGE_BITS)%Z.
+  { exact: numbers.align_up_below. }
+  rewrite /page_align_up; lia.
+Qed.
+
+Definition page_offset_of (addr : N) := (addr - page_align_dn addr)%N.
+Definition qPAGE_SIZE : Qp := N_to_Qp PAGE_SIZE.
+
+Lemma page_align_dn_offset_of addr :
+  addr = (page_align_dn addr + page_offset_of addr)%N.
+Proof.
+  rewrite /page_offset_of comm_L N.sub_add //.
+  exact: page_align_dn_below.
+Qed.
+
+Lemma page_align_ofs_eq a1 a2 :
+  page_align_dn a1 = page_align_dn a2 ->
+  page_offset_of a1 = page_offset_of a2 ->
+  a1 = a2.
+Proof.
+  intros h1 h2.
+  by rewrite (page_align_dn_offset_of a1) (page_align_dn_offset_of a2) h1 h2.
+Qed.
+
+Definition page_offset (va : vaddr) : vaddr * N :=
+  (page_align_dn va, page_offset_of va).
 
 (** Page attributes *)
 Module attrs.
@@ -25,6 +90,9 @@ Definition opteT : Set := option pteT.
 
 #[deprecated(note="")]
 Notation user := uexec (only parsing).
+
+Definition attrs_none : attrs.t := attrs.Build_t false false false false.
+Definition attrs_W := attrs.Build_t false true false false.
 
 Definition R : t :=
 {| read := true ; write := false ; sexec := false ; uexec := false |}.
