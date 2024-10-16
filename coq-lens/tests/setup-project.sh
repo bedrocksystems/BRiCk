940de262a2b3e6bec9eef3f2eab5cf61539ec04c
Rodolphe Lepigre <rodolphe@bluerock.io>
2024-10-16 15:26:32 +0200

    Fix cram tests.

--- coq-lens/tests/setup-project.sh
+++ coq-lens/tests/setup-project.sh
@@ -10,4 +10,5 @@ cat > dune-project <<EOF
 EOF
 
 export COQPATH="$DUNE_SOURCEROOT/_build/install/default/lib/coq/user-contrib"
+export COQLIB="$DUNE_SOURCEROOT/_build/install/default/lib/coq"
 export DUNE_CACHE=disabled
