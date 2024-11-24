a6c75054367ca89295392fb065b82aab5f86448b
Gregory Malecha <gregory@bluerock.io>
2024-11-24 07:50:17 -0500

    Add missing license.

--- theories/lang/cpp/syntax/supported.v
+++ theories/lang/cpp/syntax/supported.v
@@ -1,3 +1,9 @@
+(*
+ * Copyright (c) 2024 BlueRock Security, Inc.
+ * This software is distributed under the terms of the BedRock Open-Source License.
+ * See the LICENSE-BedRock file in the repository root for details.
+ *)
+
 Require Import bedrock.prelude.base.
 Require Import bedrock.lang.cpp.syntax.core.
 Require Import bedrock.lang.cpp.syntax.translation_unit.
