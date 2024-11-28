18e68aae2113a528a70a25d125152262ebab778c
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2024-11-28 17:53:36 +0100

    Extend is_arithmetic to support floats
    Needed to fix typechecking of negations, which shows up in the Mac/clang
    standard library.

--- rocq-bluerock-brick/tests/cpp2v/test_float_lit.t/test.cpp
+++ rocq-bluerock-brick/tests/cpp2v/test_float_lit.t/test.cpp
@@ -3,6 +3,11 @@
  * This software is distributed under the terms of the BedRock Open-Source License.
  * See the LICENSE-BedRock file in the repository root for details.
  */
+float max();
+float
+lowest() {
+  return -max();
+}
 
 void foo() {
 	float f = 1.0f;
