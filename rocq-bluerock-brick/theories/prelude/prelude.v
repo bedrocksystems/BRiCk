34c5090238d7bbf048bd9d779a242a8219d4fe5f
Hai Dang <hai@bluerock.io>
2024-12-11 15:52:25 +0100

    Extend prelude.prelude

--- rocq-bluerock-brick/theories/prelude/prelude.v
+++ rocq-bluerock-brick/theories/prelude/prelude.v
@@ -4,11 +4,29 @@
  * This software is distributed under the terms of the BedRock Open-Source License.
  * See the LICENSE-BedRock file in the repository root for details.
  *)
+
 (** Small extensions to stdpp. *)
 Require Export bedrock.prelude.base.
+
+Require Export bedrock.prelude.bool.
+
 Require Export bedrock.prelude.fin.
-Require Export bedrock.prelude.numbers.
-Require Export bedrock.prelude.sets.
+Require Export bedrock.prelude.fin_map_dom.
+Require Export bedrock.prelude.fin_maps.
 Require Export bedrock.prelude.fin_sets.
+
+Require Export bedrock.prelude.gmap.
+
+Require Export bedrock.prelude.finite.
+
 Require Export bedrock.prelude.list.
 Require Export bedrock.prelude.list_numbers.
+
+Require Export bedrock.prelude.numbers.
+
+Require Export bedrock.prelude.option.
+
+Require Export bedrock.prelude.propset.
+
+Require Export bedrock.prelude.sets.
+Require Export bedrock.prelude.relations.
