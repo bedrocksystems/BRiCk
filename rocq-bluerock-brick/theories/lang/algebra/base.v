855526d4fd452ebf0d2b38cd0fec3be4012d9106
Hai Dang <hai@bluerock.io>
2024-12-11 11:57:41 +0100

    preludes for lang.algebra

---
+++ rocq-bluerock-brick/theories/lang/algebra/base.v
@@ -0,0 +1,14 @@
+(*
+ * Copyright (C) 2024 BlueRock Security, Inc.
+ *
+ * This software is distributed under the terms of the BedRock Open-Source License.
+ * See the LICENSE-BedRock file in the repository root for details.
+ *)
+
+(* This exports basic facilities for defining CMRAs *)
+Require Export iris.algebra.cmra.
+Require Export iris.algebra.updates.
+Require Export iris.algebra.local_updates.
+
+Require Export iris.algebra.excl.
+Require Export iris.algebra.auth.
