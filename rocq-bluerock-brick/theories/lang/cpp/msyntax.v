54edf0d09b41d28219e18a8018f0f448c328d6cb
Gregory Malecha <gregory@bluerock.io>
2024-12-12 15:08:00 -0500

    Test cases for template type generation.

---
+++ rocq-bluerock-brick/theories/lang/cpp/msyntax.v
@@ -0,0 +1,9 @@
+(*
+ * Copyright (c) 2024 BlueRock Security, Inc.
+ * This software is distributed under the terms of the BedRock Open-Source License.
+ * See the LICENSE-BedRock file in the repository root for details.
+ *)
+
+Require Export bedrock.lang.cpp.syntax.
+Require Export bedrock.lang.cpp.syntax.mcore.
+Require Export bedrock.lang.cpp.syntax.templates.
